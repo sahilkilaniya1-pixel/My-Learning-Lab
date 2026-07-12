636acc8b3e2a679268096ad087cc8a432ed10f27
Sahil Golu <sahilkilaniya1@gmail.com>
2026-07-12 18:57:29 +0530

    commit

--- All lang-Practice/practice.js
+++ All lang-Practice/practice.js
@@ -7,4 +7,68 @@ const usersDataset = [
   { name: "Bob", age: 19, isActive: true },
   { name: "Alice", age: 25, isActive: true }
 ];
+const ascendingArray= [...numbersArray].sort((x,y)=>x-y);
+console.log(ascendingArray);
 
+const descendingArray=[...numbersArray].sort((x,y)=> y-x);
+console.log(descendingArray);
+
+
+const sortedDesc = [...numbersArray].sort((x,y)=> y-x);
+const productOfTowlargest =sortedDesc[0]* sortedDesc[1];
+console.log(productOfTowlargest);
+
+
+const sortedAsc = [...numbersArray].sort((x,y) => x-y);
+const productOfTowsmallest = sortedAsc[0]* sortedAsc[sortedAsc.length-1];
+console.log(productOfTowlargest);
+
+
+const serializedString = mixedArray.join(",");
+console.log(serializedString);
+
+
+const stringSample = "sahil";
+const deserializedArray = stringSample.split("");
+console.log(deserializedArray);
+
+const transformationResult = namesList.map(name => name.length);
+console.log(transformationResult);
+
+
+const matchUser = usersDataset.find(user => user.isActive === true && user.age<18);
+console.log(matchUser);
+
+
+const functioncheck = usersDataset.every(user => user.isActive === true);
+console.log(functioncheck);
+
+
+const existenceEvaluation = usersDataset.some(user => user.age < 18);
+console.log(existenceEvaluation);
+
+
+const filterSubset =usersDataset.filter(user => user.isActive ===true);
+console.log(filterSubset);
+
+
+const expenseList = [100, 250, 500, 80];
+const runningGrandTotal = expenseList.reduce((accumulator, current) => accumulator + current, 0);
+console.log(runningGrandTotal);
+
+
+console.log("13. Stream Processing Output Initialization:");
+fruits.forEach((fruit, index) => {
+  console.log(` -> Element detected at positional index [${index}]: ${fruit}`);
+});
+
+
+
+const genericScore = [45,72, 98, 60];
+const targetIndexResult = genericScore.findIndex(score => score <=75);
+console.log(targetIndexResult);
+
+
+const logisticCart = ["laptop", "Mouse", "keyboard"];
+console.log(logisticCart.includes("trimer"));
+console.log(logisticCart.includes("mouse"));
