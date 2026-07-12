07a89fbeaf0fb16e71acba054634fb0db4fa0c3c
Sahil Golu <sahilkilaniya1@gmail.com>
2026-07-12 16:28:50 +0530

    commit

---
+++ All lang-Practice/practice.js
@@ -0,0 +1,10 @@
+const numbersArray = [2, 4, 6, 2, 0, 56, 32, 65, 78, 65, 78, 95];
+const mixedArray = [2, 4, 5, 3, 5, 3, 2, 5, "a", "b"];
+const namesList = ["Aayush", "Sahil", "Akshya"];
+const usersDataset = [
+  { name: "John", age: 17, isActive: true },
+  { name: "Jane", age: 22, isActive: false },
+  { name: "Bob", age: 19, isActive: true },
+  { name: "Alice", age: 25, isActive: true }
+];
+
