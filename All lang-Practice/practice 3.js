5d3d14392d2e5227c39d850a0a39cad36433badb
Sahil Golu <sahilkilaniya1@gmail.com>
2026-07-13 18:34:37 +0530

    DAY 18 JAVASCRIPT QUESTION

--- All lang-Practice/practice 3.js
+++ All lang-Practice/practice 3.js
@@ -84,3 +84,21 @@ function sayHi(){
     };
 };
 console.log(sayHi());
+
+
+
+function reverseStringMannual(str){
+    let reversed = "";
+    for(let i =str.length -1; i>=0; i--){
+        reversed = reversed + str[i];
+    } 
+    return reversed;
+}
+console.log(reverseStringMannual("sahil"));
+
+
+
+function reverseStringbuiltin(str){
+    return str.split("").reverse().join("");
+}
+console.log(reverseStringMannual("sahil"));
