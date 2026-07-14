db9d3b35ee8fbf0c3aab77feb3c295256d0ee03d
Sahil Golu <sahilkilaniya1@gmail.com>
2026-07-14 23:44:25 +0530

    commit

--- All lang-Practice/practice 6.js
+++ All lang-Practice/practice 6.js
@@ -5,3 +5,15 @@ if(withdrawalAmount <= balance){
 } else{
     console.log("paise kam hain");
 }
+
+
+let marks = 88;
+if(marks >=90){
+    console.log("Grade A");
+} else if(marks >= 75) {
+    console.log("Grade B");
+} else if(marks >= 35) {
+    console.log("Grade C");
+} else {
+    console.log("Fail");
+}
