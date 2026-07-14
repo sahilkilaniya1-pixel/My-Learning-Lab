78586cf9ca767622b97c1b98eef3b4199edb7577
Sahil Golu <sahilkilaniya1@gmail.com>
2026-07-14 21:48:26 +0530

    comit

---
+++ All lang-Practice/practice4.js
@@ -0,0 +1,43 @@
+// let var const
+//  mostly use let
+let age = 20;
+age = 21;
+age= 25;
+console.log(age);
+
+
+const pi = 3.14;
+// pi = 3.15;   show error becuase const value cant update
+console.log(pi);
+
+
+let score = 20;
+let price = 20.22;
+console.log(score,price); // both result show
+
+
+let name = "sahil";
+console.log(name);
+
+
+letisLoggedIn = true;
+console.log(letisLoggedIn);
+
+
+let x;
+console.log(x); // undefined
+
+let data = null;
+console.log(data);  // null
+
+
+let score1 = 100;  // number
+let name1 = "amit";  //string
+console.log(typeof score);
+console.log(typeof name);
+
+
+let result = 10 + "20";  //1020  concatenate
+console.log(result);
+let dynamic = "5" -2;   // output 3
+console.log(dynamic);
