d84a7c54008dfe4a308f4b51d31e64393da3d965
Sahil Golu <sahilkilaniya1@gmail.com>
2026-07-14 17:21:06 +0530

    commit

--- All lang-Practice/practice 4.js
+++ All lang-Practice/practice 4.js
@@ -1,8 +1,33 @@
-function reverseStringManual(str) {
-  let reversed = "";
-  for (let i = str.length - 1; i >= 0; i--) {
-    reversed = reversed + str[i];
-  }
-  return reversed;
+function reverseWord(str){
+    let reversed = "";
+    for(let i = str.length-1; i>=0; i--){
+        reversed = reversed+str[i];
+    }
+    return reversed;
 }
-console.log(reverseStringManual("sahil"));
+console.log(reverseWord("sahil"));
+
+function reversedWordBuiltin(str){
+    return str.split("").reverse().join("");
+}
+console.log(reversedWordBuiltin("sahil"));
+
+function countword(str, char){
+    let count =0;
+    for(let i = 0; i<str.length; i++){
+        if(str [i]=== char){
+            count ++;
+        }
+    }
+    return count;
+}
+console.log(countword("sahil", "a"));
+console.log(countword("sahil", "l"));
+
+function countchar(str, char){
+    return str.split(char).length -1;
+}
+console.log(countword("sahil", "a"));
+
+
+
