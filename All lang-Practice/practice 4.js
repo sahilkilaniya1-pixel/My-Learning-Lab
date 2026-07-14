e56b5710f073057bd85294df52a7c0c42d5b20cc
Sahil Golu <sahilkilaniya1@gmail.com>
2026-07-14 17:01:37 +0530

    commit

---
+++ All lang-Practice/practice 4.js
@@ -0,0 +1,8 @@
+function reverseStringManual(str) {
+  let reversed = "";
+  for (let i = str.length - 1; i >= 0; i--) {
+    reversed = reversed + str[i];
+  }
+  return reversed;
+}
+console.log(reverseStringManual("sahil"));
