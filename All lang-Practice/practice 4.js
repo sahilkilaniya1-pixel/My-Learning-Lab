e391f1381e691bac141d326de87803a99b7f80d1
Sahil Golu <sahilkilaniya1@gmail.com>
2026-07-14 18:58:51 +0530

    commit

--- All lang-Practice/practice 4.js
+++ All lang-Practice/practice 4.js
@@ -83,10 +83,24 @@ function hasKeybuitlin(obj, searchKey){
 }
 console.log(hasKeyManual({name:"sahil",age: 20},"age"));
 
+function doubleNummanual(...arr){
+    let result = [];
+    for(let i =0; i< arr.length; i++){
+        if(arr[i] % 2 ===0){
+            result.push(arr[i]* 2);
+        } else {
+            result.push(arr[i]);
+        }
+    }
+    return result;
+}
 
+console.log(doubleNummanual(1,2,3,4,5)); 
 
-
-
+function doubleNummanualBuilt(...arr){
+    return arr.map(num => num% 2===0 ? num* 2: num);
+}
+console.log(doubleNummanualBuilt(1,2,3,4,5)); 
 
 
 
