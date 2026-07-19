c84536cae87affc18f08b17178db3b24c505a267
Sahil Golu <sahilkilaniya1@gmail.com>
2026-07-19 16:39:17 +0530

    commit

---
+++ Python-Notes/1.Variables aur Data Types.py
@@ -0,0 +1,4 @@
+# computer ko koi bhi calculation karne ke liye memory mein data save karna padta hain. Python mein data save karne ke liye hum Variables ka use karte hain. Aap varibale ko "Box" ki tarah samajh sakte hain jiske upar aap ek naam likhte hain aur andar koi value daal dete hain.
+
+# Python mein alag-alag tarah ka data store karne ke liye Data-Type hote hain:
+
