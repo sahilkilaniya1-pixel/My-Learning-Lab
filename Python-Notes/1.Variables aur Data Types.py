3cc5381829a9bce0065ea0918bfe3e6adc947ec7
Sahil Golu <sahilkilaniya1@gmail.com>
2026-07-19 17:54:44 +0530

    commit

--- Python-Notes/1.Variables aur Data Types.py
+++ Python-Notes/1.Variables aur Data Types.py
@@ -2,3 +2,15 @@
 
 # Python mein alag-alag tarah ka data store karne ke liye Data-Type hote hain:
 
+# :: Integer(int): poore number (bina decimal ke )- jaise 10, -5, 100.
+# :: Float: Decimal wale numbers - jaise 10.5, 20,5, -0.5
+# :: String(str): Text ya words.Inhe hamesha quotes "" ya '' ke andar likha jata hai -- jaise "Rahul". 'python 2026.
+# :: Boolean (bool) : Sirf do values - True (sahi) ya False (galat).
+
+age = 25;     #Integer
+prices = 99.99  #Float
+name = "sahil"  #String
+is_learning = True  #Boolean
+
+print(name)
+print(age)
