f0130775c9d43955cb652012d491e39b0399adcc
Sahil Golu <sahilkilaniya1@gmail.com>
2026-07-19 21:59:15 +0530

    commit

---
+++ Python-Notes/2.Operators.py
@@ -0,0 +1,19 @@
+# Jab data box (variable) mein aa gaya, to uspar kaam bhi karna hoga. Iske liye hum Operators ka use karte hain.
+
+# :: Arithmetic Operators: Math karne ke liye - +(plus),-(minus)*(multiply),/(divide).
+# :: Comparison Operators: Do cheezon ko compare karne ke liye. Inka answer hamesha True ya False aata hai:
+
+# --> (kya dono barabar hain?)
+# --> (Kya dono barabar nahi hain?)
+# --> (Greater than / bada hai)
+# --> (Less than / chhota hai)
+
+# CODE EXAMPLE::
+a = 10
+b = 3
+
+sum_result = a + b    #13
+is_greater = a > 5    #True
+
+print(sum_result)
+print(is_greater)
