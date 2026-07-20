611a87abbbb660ae269644557e2daa4ed6b49dd8
Sahil Golu <sahilkilaniya1@gmail.com>
2026-07-20 21:38:34 +0530

    commit

--- Python-Notes/4.conditional statement.py
+++ Python-Notes/4.conditional statement.py
@@ -1,3 +1,9 @@
 # Ab seekhte hain computer ko decusions (faisle) lena sikhana. Jab humein condition ke hisab se code chalana ho, tab hum if aur else ka use karte hain.
 
 # Python ka sabse bada rule (identation): Python mein curly brackets nahi hote. if ke andar ky code chalega, ye batane ke liye hum line ke shuruat mein 4 spaces (ya ek Tab) ka gap dete hain.
+
+age = int(input("Enter your age: "))
+if age >=18:
+    print("Aap vote de sakte hain")
+else:
+    print("Aap abhi chhote hain, vote nahi de sakte. ")
