ef58d8bb1618ac38b594dd1d93bc32b0a59e6236
Sahil Golu <sahilkilaniya1@gmail.com>
2026-07-20 21:37:09 +0530

    commit

---
+++ Python-Notes/4.conditional statement.py
@@ -0,0 +1,3 @@
+# Ab seekhte hain computer ko decusions (faisle) lena sikhana. Jab humein condition ke hisab se code chalana ho, tab hum if aur else ka use karte hain.
+
+# Python ka sabse bada rule (identation): Python mein curly brackets nahi hote. if ke andar ky code chalega, ye batane ke liye hum line ke shuruat mein 4 spaces (ya ek Tab) ka gap dete hain.
