c48d50845ec3247ccff9d3100061d1c1f2d9ba73
Sahil Golu <sahilkilaniya1@gmail.com>
2026-07-20 18:19:26 +0530

    commit

---
+++ Python-Notes/3.User Input.py
@@ -0,0 +1,7 @@
+# Agar Aap chahate hain ki aapka program user se baat kare aur unse kuch pooche, to tum intput() function ka use karte hain.
+
+# zaroori baat: input() user se jo bhi leta hain. use hamesha String (Text) samajhta hai. Agar aapko numbers par math karni hai. to aapko user int() ya float() lagakar number mein badalna padega (isse hum Type Casting khete hain).
+
+name= input("Enter your name: ")
+age= input("Enter your age ")
+print("Hello"+ name, age)
