e1cd86fdf55b1976986b4779a2e85b7c067d880a
Sahil Golu <sahilkilaniya1@gmail.com>
2026-07-20 22:50:40 +0530

    commit

--- Python-Notes/6.DataStructures & Function.py
+++ Python-Notes/6.DataStructures & Function.py
@@ -2,3 +2,7 @@
 # Abhi tak hum ek varibale mein sirf ek he value save kar pa rahe the (jaise age = 25).Agar aapko apni class ke 5 students ke naam ya 10 products ki prices save karni hon, to aap alag-alag variables nahi banayenge. Unke liye hum List use karte hain.
 
 # List ko hamesha square brackets[] ka andar likha jata hai, aur items ko comma, se alag karte hain.
+
+fruits = ["apple","Banana","Mango","Orange"]
+print(fruits[0])
+print(fruits[1])
