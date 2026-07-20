0c3f898c33ff179c26b31ad1c73f1955b24d8efb
Sahil Golu <sahilkilaniya1@gmail.com>
2026-07-20 22:15:09 +0530

    commit

--- Python-Notes/5.Loops.py
+++ Python-Notes/5.Loops.py
@@ -2,3 +2,8 @@
 
 # A) For Loop (Ginti ke hisab se chalane waala):
 # Agar aapko phele se pata ho ki loop kitni baar chalana hai, to for loop aur range () ka use karte hain.
+
+# Note: range(1,6) ka matlab hai 1 se shuru karo aur 6 se ek phele (yaani 5) tak jao.
+
+for i in range (1,6):
+    print(i)
