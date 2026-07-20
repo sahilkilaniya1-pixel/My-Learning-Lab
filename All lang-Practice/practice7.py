ea47ed4db2e17439919fd146f04116da615b8944
Sahil Golu <sahilkilaniya1@gmail.com>
2026-07-20 22:30:10 +0530

    commit

--- All lang-Practice/practice7.py
+++ All lang-Practice/practice7.py
@@ -19,12 +19,19 @@
 #     print("Odd")
 
 
-traffic_color =input("enter the color (red,yellow,green):").lower()
-if traffic_color == "red":
-    print("STOP")
-elif traffic_color == "yellow":
-  print("GET READY!")
-elif traffic_color == "green":
-    print("GO!")
-else:
-    print("invalid color")
+# traffic_color =input("enter the color (red,yellow,green):").lower()
+# if traffic_color == "red":
+#     print("STOP")
+# elif traffic_color == "yellow":
+#   print("GET READY!")
+# elif traffic_color == "green":
+#     print("GO!")
+# else:
+#     print("invalid color")
+
+
+
+num1 = float(input("Enter the first number: "))
+numb2 = float(input("Enter the second number: "))
+
+if
