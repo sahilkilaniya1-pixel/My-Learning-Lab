da7e413f34746ad3f9cf92badfd8358779df62d5
Sahil Golu <sahilkilaniya1@gmail.com>
2026-07-20 22:14:05 +0530

    commit

--- Python-Notes/5.Loops.py
+++ Python-Notes/5.Loops.py
@@ -1,1 +1,4 @@
 # Jab humein ek he code ko bina baar-baar likhe kai baar chalana ho, tab hum loop ka use karte hain. Python mein do tarah ke loops hote hain:
+
+# A) For Loop (Ginti ke hisab se chalane waala):
+# Agar aapko phele se pata ho ki loop kitni baar chalana hai, to for loop aur range () ka use karte hain.
