85332846d861de68194313dd45221dcd4bf6c5fe
Sahil Golu <sahilkilaniya1@gmail.com>
2026-07-20 18:24:16 +0530

    commit

--- Python-Notes/3.User Input.py
+++ Python-Notes/3.User Input.py
@@ -3,5 +3,5 @@
 # zaroori baat: input() user se jo bhi leta hain. use hamesha String (Text) samajhta hai. Agar aapko numbers par math karni hai. to aapko user int() ya float() lagakar number mein badalna padega (isse hum Type Casting khete hain).
 
 name= input("Enter your name: ")
-age= input("Enter your age ")
-print("Hello"+ name, age)
+# age= input("Enter your age ")
+print("Hello"+ name)
