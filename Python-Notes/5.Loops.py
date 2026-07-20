d03888a098d696e5d3921133052747b7ded8d15b
Sahil Golu <sahilkilaniya1@gmail.com>
2026-07-20 22:20:59 +0530

    commit

--- Python-Notes/5.Loops.py
+++ Python-Notes/5.Loops.py
@@ -7,3 +7,7 @@
 
 for i in range (1,6):
     print(i)
+
+
+#  B) While loop (condition ke hisab se chalne wala)
+# ye tab tak chalta retha hai jab tak iski condition Ture rethi hai. Jaise hi condition False hui, loop ruk jata hai.
