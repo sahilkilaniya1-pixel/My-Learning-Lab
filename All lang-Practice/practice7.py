ff042e8cfb7f92331ca7e30699f4784f3f47e1be
Sahil Golu <sahilkilaniya1@gmail.com>
2026-07-20 19:03:02 +0530

    commit

--- All lang-Practice/practice7.py
+++ All lang-Practice/practice7.py
@@ -1,6 +1,8 @@
-total_bill= float(input("Enter the total bill "))
-total_people =int(input("Enter the people "))
+# total_bill= float(input("Enter the total bill "))
+# total_people =int(input("Enter the people "))
 
-newbill= total_bill/total_people
+# newbill= total_bill/total_people
 
-print(newbill)
+# print(newbill)
+
+
