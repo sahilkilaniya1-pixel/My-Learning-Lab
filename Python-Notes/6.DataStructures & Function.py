f6ac0fad50cf440329005e355177377f76e4810d
Sahil Golu <sahilkilaniya1@gmail.com>
2026-07-20 22:44:10 +0530

    commit

--- Python-Notes/6.DataStructures & Function.py
+++ Python-Notes/6.DataStructures & Function.py
@@ -1,1 +1,2 @@
 #  1. Python List (Data ki Diary)
+# Abhi tak hum ek varibale mein sirf ek he value save kar pa rahe the (jaise age = 25).Agar aapko apni class ke 5 students ke naam ya 10 products ki prices save karni hon, to aap alag-alag variables nahi banayenge. Unke liye hum List use karte hain.
