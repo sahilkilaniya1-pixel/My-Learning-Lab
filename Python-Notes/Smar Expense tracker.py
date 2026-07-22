576cfe8d2b89a4e50f7f1fe72cc12e83273b4de6
Sahil Golu <sahilkilaniya1@gmail.com>
2026-07-22 16:53:25 +0530

    commit

--- Python-Notes/Smar Expense tracker.py
+++ Python-Notes/Smar Expense tracker.py
@@ -1,3 +1,7 @@
 # Aapne zero se shuruat karke poori Python Journey cover kar li hai. Ab time hain sabhi modules (Loops, Functions, Files/Json, OOPs)ko ek saath use karke Grand Final Project banane ka.
 
 # 1.requirement & Features
+# Ek complete application jo aapke daily/monthly kharche(expenses)track kare.
+# Add Expense:Amount($), Category(Food,rent,shopping),aur Description
+# Save Data: Data .json ya.txt file mein save ho taaki app restart hone par bhi kharchon ka record safe rahe.
+# 
