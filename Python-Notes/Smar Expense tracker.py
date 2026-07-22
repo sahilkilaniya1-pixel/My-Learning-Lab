92245b40f50369fa0dc63ac3c0773cf869729dde
Sahil Golu <sahilkilaniya1@gmail.com>
2026-07-22 16:48:53 +0530

    commit

--- Python-Notes/Smar Expense tracker.py
+++ Python-Notes/Smar Expense tracker.py
@@ -1,1 +1,3 @@
 # Aapne zero se shuruat karke poori Python Journey cover kar li hai. Ab time hain sabhi modules (Loops, Functions, Files/Json, OOPs)ko ek saath use karke Grand Final Project banane ka.
+
+# 1.requirement & Features
