8992ace684e3ce61b8d06db0e02e5f050775f435
Sahil Golu <sahilkilaniya1@gmail.com>
2026-07-22 15:32:15 +0530

    commit

--- Python-Notes/ATM PROJECT.py
+++ Python-Notes/ATM PROJECT.py
@@ -12,3 +12,8 @@
 # deposit(amout): account mein paise add kare aur naya balance bataye.
 # withdraw(amount):Account se paise nikale.
     # Validation: Agar user balance se zayda paise nikalne ki koshish kare(e.g.balance 1000 hai aur withdraw 5000 karna chahe).to dikhaiye: "Insufficient Balance! Aapke pass itne paise nahi hain."
+
+
+# 3.Main program loop:
+# Class ka ek object banaiye(e.g. user_acc = Account ("Sahil",5000)).
+# User ko menu dikhaiye: 1.Check Balance, 2.Deposit, 3.withdraw, 4.Exit.
