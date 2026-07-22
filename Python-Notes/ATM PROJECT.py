d56ce1f355b6eea9af98149bf14b3da3cd139d83
Sahil Golu <sahilkilaniya1@gmail.com>
2026-07-22 15:25:26 +0530

    commit

--- Python-Notes/ATM PROJECT.py
+++ Python-Notes/ATM PROJECT.py
@@ -2,3 +2,9 @@
 
 # Project Requirements:
 # aapko ek Account naam ki class banani hai.
+
+# 1.Attributes(__init__ ke andar):
+# holder_name(User ka naam)
+# balance(Starting paise, e.g.1000)
+
+# 2.
