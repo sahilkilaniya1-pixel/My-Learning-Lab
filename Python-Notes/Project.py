1c15505d73cfd096461e7697b8c0a70fc61639c7
Sahil Golu <sahilkilaniya1@gmail.com>
2026-07-22 16:19:38 +0530

    commit

--- Python-Notes/Project.py
+++ Python-Notes/Project.py
@@ -1,2 +1,4 @@
+# 1.:: pip aur External Libraries
+# Python ke pass hazaaron ready-made packages hain jo doosre developers ne banaye hain. Inhe inst
 # Terminal ya Command Prompt mein aise install karte hain:
 # pip install requests
