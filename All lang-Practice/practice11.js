a6f9acc42669f5090af37c4c19bc82430010cbcd
Sahil Golu <sahilkilaniya1@gmail.com>
2026-07-25 23:49:32 +0530

    commit

--- All lang-Practice/practice11.js
+++ All lang-Practice/practice11.js
@@ -1,18 +1,11 @@
 let n = 5;
 
-for (let i = 1; i <= n; i++) {
+for (let i = n; i >= 1; i--) {
   let row = "";
-
-  // 1. Pehle spaces add karo
   for (let j = 1; j <= n - i; j++) {
-    row += "";
-  }
-
-  // 2. Phir stars add karo
-  for (let k = 1; k <= i; k++) {
     row += " ";
   }
-  for(let l =2; l<=i; l++ ){
+  for (let k = 1; k <= i; k++) {
     row += "*";
   }
 
