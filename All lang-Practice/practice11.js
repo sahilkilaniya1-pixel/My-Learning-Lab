2aee27554a26221b18325a4d4bea78f42061f03c
Sahil Golu <sahilkilaniya1@gmail.com>
2026-07-25 19:34:30 +0530

    commit

--- All lang-Practice/practice11.js
+++ All lang-Practice/practice11.js
@@ -1,5 +1,3 @@
-
-
 // // Question 2. Step-by-Step Code Walkthrough
 // let s = 5;
 // for(let i=0; i<=s; i++){
@@ -24,15 +22,15 @@
 //     console.log(row);
 // }
 
- let s = 5;
- for(let i=0; i<=s; i++){
-    let row="*";
-    
-    for(let j=1; j<=s-1; j++){
-        row+="*";
-    }
-    for(let j=1; j<=i; j++){
-        row+="";
-    }
-    console.log(row);
- }
+// let s = 5;
+// for (let i = 0; i <= s; i++) {
+//   let row = "";
+
+//   for (let j = 1; j <= s - 1; j++) {
+//     row += "";
+//   }
+//   for (let j = 1; j <= i; j++) {
+//     row += "*";
+//   }
+//   console.log(row);
+// }
