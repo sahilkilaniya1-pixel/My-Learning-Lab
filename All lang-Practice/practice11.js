1669d257fc75498ea5cdd6069a04bd15d08846bc
Sahil Golu <sahilkilaniya1@gmail.com>
2026-07-25 21:52:31 +0530

    commit

--- All lang-Practice/practice11.js
+++ All lang-Practice/practice11.js
@@ -1,56 +1,8 @@
-// // Question 2. Step-by-Step Code Walkthrough
-// let s = 5;
-// for(let i=0; i<=s; i++){
-//     let row="";
-//     for(let j=0; j<=i; j++){
-//         row +="*";
-//     }
-//     console.log(row);
-// }
-
-// // Quesiton 3: Spaces wala pattern(Advanced Logic)
-// let n  =5;
-// for(let i=0; i<=n; i++){
-//     let row="";
-
-//     for(let k =1; k<=n-1; k++){
-//         row += " ";
-//     }
-//     for(let j =1; j<=i; j++){
-//         row+= "*";
-//     }
-//     console.log(row);
-// }
-
-// let s = 5;
-// for (let i = 0; i <= s; i++) {
-//   let row = "";
-
-//   for (let j = 1; j <= s - 1; j++) {
-//     row += "";
-//   }
-//   for (let j = 1; j <= i; j++) {
-//     row += "*";
-//   }
-//   console.log(row);
-// }
-
-// let n = 5;
-// for (let i = n; i >= 1; i--) {
-//   let row = "";
-
-//   for (let j = 1; j <= i; j++) {
-//     row += "*";
-//   }
-//   console.log(row);
-// }<
-
 let n = 5;
-for(let i=0; i>=i; i++){
+for (let i = 0; i <= n; i++) {
   let row = "";
-
-  for(let j=1; j>=i; i++){
-    row +="*";
+  for (let j = 1; j <= i; j++) {
+    row += "*";
   }
   console.log(row);
 }
