3f8b2c0797772280cdce887641a6efed906cceab
Sahil Golu <sahilkilaniya1@gmail.com>
2026-07-25 19:27:09 +0530

    COMMIT

--- All lang-Practice/practice11.js
+++ All lang-Practice/practice11.js
@@ -1,30 +1,32 @@
-/*
-outer loop we will use (i) ye tay karta hai ki total kitni line or rows print hongi.(i loop)
+// /*
+// outer loop we will use (i) ye tay karta hai ki total kitni line or rows print hongi.(i loop)
 
-but for innr loop we will use (j)  ye tay karta hai ki har line ke andar kitne star * ya spaces print honge (j loop).
-*/
+// but for innr loop we will use (j)  ye tay karta hai ki har line ke andar kitne star * ya spaces print honge (j loop).
+// */
 
 
-// Question 2. Step-by-Step Code Walkthrough
-let s = 5;
-for(let i=0; i<=s; i++){
-    let row="";
-    for(let j=0; j<=i; j++){
-        row +="*";
-    }
-    console.log(row);
-}
+// // Question 2. Step-by-Step Code Walkthrough
+// let s = 5;
+// for(let i=0; i<=s; i++){
+//     let row="";
+//     for(let j=0; j<=i; j++){
+//         row +="*";
+//     }
+//     console.log(row);
+// }
 
-// Quesiton 3: Spaces wala pattern(Advanced Logic)
-let n  =5;
-for(let i=0; i<=n; i++){
-    let row="";
+// // Quesiton 3: Spaces wala pattern(Advanced Logic)
+// let n  =5;
+// for(let i=0; i<=n; i++){
+//     let row="";
 
-    for(let k =1; k<=n-1; k++){
-        row += " ";
-    }
-    for(let j =1; j<=i; j++){
-        row+= "*";
-    }
-    console.log(row);
-}
+//     for(let k =1; k<=n-1; k++){
+//         row += " ";
+//     }
+//     for(let j =1; j<=i; j++){
+//         row+= "*";
+//     }
+//     console.log(row);
+// }
+
+
