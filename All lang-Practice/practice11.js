d9c596513686deac1949d10365d3b9661b0d0efd
Sahil Golu <sahilkilaniya1@gmail.com>
2026-07-25 19:22:02 +0530

    commit

--- All lang-Practice/practice11.js
+++ All lang-Practice/practice11.js
@@ -5,12 +5,26 @@ but for innr loop we will use (j)  ye tay karta hai ki har line ke andar kitne s
 */
 
 
-
-let n = 5;
-for(let i=0; i<=n; i++){
+// Question 2. Step-by-Step Code Walkthrough
+let s = 5;
+for(let i=0; i<=s; i++){
     let row="";
     for(let j=0; j<=i; j++){
         row +="*";
+    }
+    console.log(row);
+}
+
+// Quesiton 3: Spaces wala pattern(Advanced Logic)
+let n  =5;
+for(let i=0; i<=n; i++){
+    let row="";
+
+    for(let k =1; k<=n-1; k++){
+        row += " ";
+    }
+    for(let j =1; j<=i; j++){
+        row+= "*";
     }
     console.log(row);
 }
