cb897f7bb8df09642dc35144caf92592c0049839
Sahil Golu <sahilkilaniya1@gmail.com>
2026-07-25 17:51:47 +0530

    commit

--- All lang-Practice/practice11.js
+++ All lang-Practice/practice11.js
@@ -16,3 +16,11 @@ but for innr loop we will use (j)  ye tay karta hai ki har line ke andar kitne s
 
 
 
+let n = 5;
+for(let i=0; i<=n; i++){
+    let row ="";
+    for(let j =0; j<=i; j++){
+        row+= "*";
+    }
+    console.log(row);
+}
