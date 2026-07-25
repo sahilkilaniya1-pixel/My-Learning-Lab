9d634c8bc45f0ef9ae3260692c57ae53154c2fa6
Sahil Golu <sahilkilaniya1@gmail.com>
2026-07-25 19:48:16 +0530

    commit

--- All lang-Practice/practice11.js
+++ All lang-Practice/practice11.js
@@ -34,3 +34,13 @@
 //   }
 //   console.log(row);
 // }
+
+let n = 5;
+for (let i = n; i >= 1; i--) {
+  let row = "";
+
+  for (let j = 1; j <= i; j++) {
+    row += "*";
+  }
+  console.log(row);
+}
