d50f7fef92e19439cded834ee0c420ee3844bd83
Sahil Golu <sahilkilaniya1@gmail.com>
2026-07-25 19:16:34 +0530

    commit

--- All lang-Practice/practice11.js
+++ All lang-Practice/practice11.js
@@ -18,9 +18,9 @@ but for innr loop we will use (j)  ye tay karta hai ki har line ke andar kitne s
 
 let n = 5;
 for(let i=0; i<=n; i++){
-    let row ="";
-    for(let j =0; j<=i; j++){
-        row+= "*";
+    let row="";
+    for(let j=0; j<=i; j++){
+        row +="*";
     }
     console.log(row);
 }
