ab60578ce97e0ac84b05efa5fcac815f3dd6d673
Sahil Golu <sahilkilaniya1@gmail.com>
2026-07-26 06:37:48 +0530

    commit

--- All lang-Practice/practice11.js
+++ All lang-Practice/practice11.js
@@ -1,8 +1,12 @@
-let n =5;
-for(let i=0; i<=n; i++){
-    let row ="";
-    for(let j =1; j<=n-i-1; j++){
-        row +="*";
-    }
-    console.log(row);
+let  n=5;
+for(let i = 1; i<=n; i++){
+  let row ="";
+
+  for(let s =1; s<=n-1; s++){
+    row +="";
+  }
+  for(let j =1; j<=2*i-1; j++){
+    row +="*";
+  }
+  console.log(row)
 }
