10bf38e4a12179e31fc546448a583e9af95627f8
Sahil Golu <sahilkilaniya1@gmail.com>
2026-07-26 18:36:13 +0530

    commit

--- All lang-Practice/practice11.js
+++ All lang-Practice/practice11.js
@@ -121,4 +121,25 @@
 //   console.log(row);
 // }
 
+// let n = 5;
+// for (let i = 1; i <= n; i++) {
+//   let row = "";
+//   for (let s = 1; s <= n - i; s++) {
+//     row += " ";
+//     for (let j = 1; j <= 2 * i - 1; j++) {
+//       row += "*";
+//     }
+//   }
+//   console.log(row);
+// }
 
+// for(let i=n-1; i>=i; i--){
+//     let row ="";
+//     for(let s =i; s<=n-i; s++){
+//         row+=" ";
+//     for (let j=1; j<= 2*i-1; j++){
+//         row+="*";
+//     }
+//     }
+//     console.log(row);
+// }
--- All lang-Practice/tempCodeRunnerFile.js
+++ All lang-Practice/tempCodeRunnerFile.js
@@ -1,12 +1,22 @@
 let n = 5;
-for (let i = 1; i <= n; i++) {
-  let row = "";
+// for (let i = 1; i <= n; i++) {
+//   let row = "";
+//   for (let s = 1; s <= n - i; s++) {
+//     row += " ";
+//     for (let j = 1; j <= 2 * i - 1; j++) {
+//       row += "*";
+//     }
+//   }
+//   console.log(row);
+// }
 
-  for (let s = 1; s <= 2 *(n-i+1)-1; s++) {
-    row += " ";
-  }
-  for (let j = 1; j <= i; j++) {
-    row += "*";
-  }
-  console.log(row);
-}
+// for(let i=n-1; i>=i; i--){
+//     let row ="";
+//     for(let s =i; s<=n-i; s++){
+//         row+=" ";
+//     for (let j=1; j<= 2*i-1; j++){
+//         row+="*";
+//     }
+//     }
+//     console.log(row);
+// }
