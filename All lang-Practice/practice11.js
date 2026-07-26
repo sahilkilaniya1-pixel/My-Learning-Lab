58b96b7aa04b379f4bb1030dd9b53acfa983e804
Sahil Golu <sahilkilaniya1@gmail.com>
2026-07-26 17:57:04 +0530

    commit

--- All lang-Practice/practice11.js
+++ All lang-Practice/practice11.js
@@ -108,18 +108,5 @@
 //   console.log(row);
 // }
 
-let n = 5;
-for (let i = 1; i <= n; i++) {
-  let row = "";
-
-  for (let s = 1; s <= n - i; s++) {
-    row += " ";
-  }
-  for (let j = 1; j <= 2 *(n-i+1)-1; j++) {
-    row += "*";
-  }
-  console.log(row);
-}
-
-
+let n =5;
 
--- All lang-Practice/tempCodeRunnerFile.js
+++ All lang-Practice/tempCodeRunnerFile.js
@@ -2,10 +2,10 @@ let n = 5;
 for (let i = 1; i <= n; i++) {
   let row = "";
 
-  for (let s = 1; s <= n - i; s++) {
+  for (let s = 1; s <= 2 *(n-i+1)-1; s++) {
     row += " ";
   }
-  for (let j = i; j <= 2 * i - 1; j++) {
+  for (let j = 1; j <= i; j++) {
     row += "*";
   }
   console.log(row);
