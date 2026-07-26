015b56267f1423ad4a3cdfe223df02353a762724
Sahil Golu <sahilkilaniya1@gmail.com>
2026-07-26 16:53:48 +0530

    commit

--- All lang-Practice/practice11.js
+++ All lang-Practice/practice11.js
@@ -71,12 +71,26 @@
 // }
 
 // Inverted Right Triangle:
-let n = 5;
-for (let i = 1; i <= n; i++) {
-  let row = "";
-  for (let j = 1; j <= n - i + 1; j++) {
-    row += "*";
-  }
-  console.log(row);
-}
+// let n = 5;
+// for (let i = 1; i <= n; i++) {
+//   let row = "";
+//   for (let j = 1; j <= n - i + 1; j++) {
+//     row += "*";
+//   }
+//   console.log(row);
+// }
 
+
+// left Pascal Triangle(Right Aligned Triangle);
+
+let n =5;
+for(let i=0; i<=n; i++){
+    let row ="";
+    for(let s=1; s<=n-i;s++){
+        row += " ";
+    }
+    for(let  j=1; j<=i; j++){
+        row +="*";
+    }
+    console.log(row);
+}
