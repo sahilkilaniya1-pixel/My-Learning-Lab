dd1fa2f90308a3ea01ceaa97adbd1570b37578f5
Sahil Golu <sahilkilaniya1@gmail.com>
2026-07-26 17:27:47 +0530

    commit

--- All lang-Practice/practice11.js
+++ All lang-Practice/practice11.js
@@ -96,15 +96,14 @@
 
 // Inverted Left Triangle:
 let n = 5;
-for(let i =1; i<=n; i++){
-    let row ="";
+for (let i = 1; i <= n; i++) {
+  let row = "";
 
-    for(let s=i; s<=n-i; s++){
-        row +=" ";
-    }
-    for(let j=n; j<=2*i-1; j++){
-        row+="*";
-    }
-    console.log(row);
+  for (let s = i; s <= n - i; s++) {
+    row += " ";
+  }
+  for (let j = 1; j <= 2 * i - 1; j++) {
+    row += "*";
+  }
+  console.log(row);
 }
-
