5326d7cf34392448c4c3ce89fff1675b39448dba
Sahil Golu <sahilkilaniya1@gmail.com>
2026-07-26 06:13:22 +0530

    commit

--- All lang-Practice/practice11.js
+++ All lang-Practice/practice11.js
@@ -1,15 +1,12 @@
 let n = 5;
 for (let i = 1; i <= n; i++) {
-    let row = "";
-    
-    // Spaces ke liye loop
-    for (let s = 1; s <= n - i; s++) {
-        row += " ";
-    }
-    // Stars ke liye loop
-    for (let j = 1; j <= i; j++) {
-        row += "*";
-    }
-    
-    console.log(row);
+  let row = "";
+
+  for (let j = 1; j <= n -i; j++) {
+    row += " ";
+  }
+  for (let s = 1; s <= i; s++) {
+    row += "*";
+  }
+  console.log(row);
 }
