a1c73ae51157b6cd0c7dce6e729818c1f487c1fb
Sahil Golu <sahilkilaniya1@gmail.com>
2026-07-26 16:38:52 +0530

    commit

--- All lang-Practice/1script.js
+++ All lang-Practice/1script.js
@@ -1,4 +1,28 @@
+// const row = 5;
+
+// for(let i=0; i<=row; i++){
+//     let str = ''
+
+//     for(let j=1; j<=i; j++){
+//         str = str+'*'
+//     }
+
+//     console.log(str);
+// }
+
+
+
 const row = 5;
-for(let i=0; i<=5; i++){
-    console.log("*");
+
+for(let i=0; i<=row; i++){
+    let str = ''
+
+    for(let j=1; j<=i; j++){
+        str = str+'*'
+    }
+
+    console.log(str);
 }
+
+
+
