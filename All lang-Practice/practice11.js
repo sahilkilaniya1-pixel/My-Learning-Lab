ebee4acf7255f1183cb3a3574ab1adfa023f4618
Sahil Golu <sahilkilaniya1@gmail.com>
2026-07-26 16:54:05 +0530

    commit

--- All lang-Practice/practice11.js
+++ All lang-Practice/practice11.js
@@ -80,17 +80,16 @@
 //   console.log(row);
 // }
 
-
 // left Pascal Triangle(Right Aligned Triangle);
 
-let n =5;
-for(let i=0; i<=n; i++){
-    let row ="";
-    for(let s=1; s<=n-i;s++){
-        row += " ";
-    }
-    for(let  j=1; j<=i; j++){
-        row +="*";
-    }
-    console.log(row);
+let n = 5;
+for (let i = 0; i <= n; i++) {
+  let row = "";
+  for (let s = 1; s <= n - i; s++) {
+    row += " ";
+  }
+  for (let j = 1; j <= i; j++) {
+    row += "*";
+  }
+  console.log(row);
 }
