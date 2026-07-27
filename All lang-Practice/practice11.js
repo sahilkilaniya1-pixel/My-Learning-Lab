057319ac0a8401582e098dea2f97cef175695c6a
Sahil Golu <sahilkilaniya1@gmail.com>
2026-07-27 09:11:52 +0530

    commit

--- All lang-Practice/practice11.js
+++ All lang-Practice/practice11.js
@@ -230,3 +230,33 @@
 //   console.log(row);
 // }
 
+// let n=5;
+// for(let i=1; i<=n; i++){
+//   let row ="";
+//   for(let j=1; j<=i; j++){
+//     row +=i+" ";
+//   }
+//   console.log(row);
+// }
+
+
+// let n=5;
+// for(let i=1; i<=n; i++){
+//   let row = "";
+//   for(let j=1; j<=i; j++){
+//     row +="*";
+//   }
+//   console.log(row);
+// }
+
+
+// Numberic pattern:
+let n = 5;
+for(let i=1; i<=n; i++){
+  let row = "";
+  
+  for(let j=1;j<=i; j++){
+    row += j+" ";
+  }
+  console.log(row);
+}
