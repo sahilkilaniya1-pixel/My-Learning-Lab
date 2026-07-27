591e8e9e07851537b758d12d14636aa7d52e379f
Sahil Golu <sahilkilaniya1@gmail.com>
2026-07-27 09:33:02 +0530

    commit

--- All lang-Practice/practice11.js
+++ All lang-Practice/practice11.js
@@ -251,12 +251,12 @@
 
 
 // Numberic pattern:
-// let n = 5;
-// for(let i=1; i<=n; i++){
-//   let row = "";
+let n =5;
+for(let i=1; i<=n; i++){
+  let row = "";
   
-//   for(let j=1;j<=i; j++){
-//     row += j+" ";
-//   }
-//   console.log(row);
-// }
+  for(let j=1; j<=i; j++){
+    row += j+" ";
+  }
+  console.log(row);
+}
