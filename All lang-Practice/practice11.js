79b4a0d1b109ff5054ae4e2197c4da75bf95e7a3
Sahil Golu <sahilkilaniya1@gmail.com>
2026-07-28 08:28:20 +0530

    commiy

--- All lang-Practice/practice11.js
+++ All lang-Practice/practice11.js
@@ -283,3 +283,15 @@
 
 
 // Pattern Type 2: Continuous Counting Pattern (Floyd's Pattern)
+let n =4;
+let count =1;
+
+for(let i=1; i<=n; i++){
+    let row ="";
+
+    for(let j=1; j<=i; j++){
+        row += count + " ";
+        count ++;
+    }
+    console.log(row);
+}
