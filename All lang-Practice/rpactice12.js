f01dd2d05777bd036e25a7960839db02c9ccb24f
Sahil Golu <sahilkilaniya1@gmail.com>
2026-07-28 17:15:38 +0530

    commit

--- All lang-Practice/rpactice12.js
+++ All lang-Practice/rpactice12.js
@@ -8,23 +8,28 @@
 //     row += "*";
 //   }
 //   console.log(row);
-  
+
 // }
 // let n = prompt("input");
 // for(let i = 1;i<n+1;n--){
 //     if (i == 1  && i==n){
 //         console.log('+');
-//     }    
+//     }
 // }
 // let age = 18 ;
 // let city  = "Rohtak";
 
 // Corrected Code
+// Input lene ke liye prompt
 let age = Number(prompt("Enter your age:"));
-let city = prompt("Enter your city:");
-
-if (age >= 18&& age< 60 ) {
-    console.log("Eligible for vote in " + city);
+let city = prompt("Enter the city:");
+if (age >= 18 && age < 60)
+    if (city === "rohtak") {
+        console.log("Eligible for vote in " + city);
+    } else {
+        console.error("Voting is only allowed for residents of Rohtak!");
+    }
 } else {
-    console.log("Not eligible for vote");
+    console.log("Not Eligible for vote due to age criteria.");
 }
+
