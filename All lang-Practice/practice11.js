77f5c1f933c94b12dac24796ca7860a67a17f19f
Sahil Golu <sahilkilaniya1@gmail.com>
2026-07-28 08:25:17 +0530

    commit

--- All lang-Practice/practice11.js
+++ All lang-Practice/practice11.js
@@ -270,3 +270,12 @@
 
 
 // Alphabet Varient (A, AB, ABC)::
+let n =5;
+for(let i=1; i<=n; i++){
+    let row = "";
+
+    for(let j=1; j<=i; j++){
+        row += String.fromCharCode(64 + j)+" ";
+    }
+    console.log(row);
+}
