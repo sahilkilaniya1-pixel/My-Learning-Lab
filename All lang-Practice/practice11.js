feb5217409be705df3cbd608b9109844b494f142
Sahil Golu <sahilkilaniya1@gmail.com>
2026-07-28 08:23:45 +0530

    commit

--- All lang-Practice/practice11.js
+++ All lang-Practice/practice11.js
@@ -257,12 +257,16 @@
 // }
 
 // Numberic Pattern B:
-let n = 5;
-for (let i = 1; i <= n; i++) {
-  let row = "";
+// let n = 5;
+// for (let i = 1; i <= n; i++) {
+//   let row = "";
 
-  for (let j = 1; j <= i; j++) {
-    row += i + " ";
-  }
-  console.log(row);
-}
+//   for (let j = 1; j <= i; j++) {
+//     row += i + " ";
+//   }
+//   console.log(row);
+// }
+
+
+
+// Alphabet Varient (A, AB, ABC)::
