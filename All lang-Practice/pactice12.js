dbce04b158283d9a11fd0c1e36884b14b84c5428
Sahil Golu <sahilkilaniya1@gmail.com>
2026-07-29 06:57:13 +0530

    commit

--- All lang-Practice/pactice12.js
+++ All lang-Practice/pactice12.js
@@ -154,9 +154,12 @@ Freezing" if temperature is less than 0.
 // } else{
 //   console.log("Hot");
 // }
-let leapyar = 2000;
-if (leapyear /4==0){
-  console.log("it is leapyear")
-} else if (leapyear /4!==0){
-  console.log("not leapyear");
+
+
+function checkLeapyear(year){
+  if((year % 4===0 && year %100 !==0)|| (year % 400 ===0)){
+    console.log(year+"is a leap year");
+  } else {
+    console.log(year +"is not a leap year");
+  }
 }
