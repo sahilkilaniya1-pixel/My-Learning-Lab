f7f6e154c34a1d0871afb7ff30aef8ba0add0d32
Sahil Golu <sahilkilaniya1@gmail.com>
2026-07-30 15:43:22 +0530

    commit

--- All lang-Practice/practice15.js
+++ All lang-Practice/practice15.js
@@ -1,11 +1,9 @@
 let n = 5;
-for (let i = 0; i <= n; i++) {
+for (let i =1; i <= n; i++) {
   let row = " ";
   for (let j = 1; j <= n; j++) {
     row += "*";
   }
-  //   for(let s=i; s<=i; s++){
-  //     row+="*";
-  //   }
   console.log(row);
 }
+
