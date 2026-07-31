5eddcb7db910f9475fbed8445771661ec5cb68ba
Sahil Golu <sahilkilaniya1@gmail.com>
2026-07-31 20:47:45 +0530

    commit

--- All lang-Practice/tempCodeRunnerFile.js
+++ All lang-Practice/tempCodeRunnerFile.js
@@ -1,3 +1,16 @@
-; s++){
+let n = 5;
+for (let i = 1; i <= n; i++) {
+  let row = "";
+
+  for (let j = 1; j <= n - i; j++) {
+    row += " ";
+  }
+  for(let k=1; k<=(2*i-1); k++){
+    if(k==1|| k==(2*i-1)||i==n){
+        row+="*";
+    }else{
         row+=" ";
     }
+  }
+  console.log(row);
+}
