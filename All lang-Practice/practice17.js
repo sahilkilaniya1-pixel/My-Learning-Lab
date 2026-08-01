06d8e5b16cc269564adba7ae50808537928c820e
Sahil Golu <sahilkilaniya1@gmail.com>
2026-08-01 10:26:48 +0530

    commit

--- All lang-Practice/practice17.js
+++ All lang-Practice/practice17.js
@@ -24,8 +24,6 @@
 //   console.log(row);
 // }
 
-
-
 // let n =5;
 // for(let i=1; i<=n; i++){
 //     let row ="";
@@ -40,15 +38,15 @@
 //     console.log(row);
 // }
 
-
-
-let n =5;
-for(let i=1; i<=n; i++){
-    let row="";
-    for(let j=1; j<=n; j++){
-        if(i==j ){
-            row+="*";
-        }
+let n = 5;
+for (let i = 1; i <= n; i++) {
+  let row = "";
+  for (let j = 1; j <= i; j++) {
+    if (i === j || i + j === n + 1) {
+      row += "*";
+    } else {
+      row += " ";
     }
-    console.log(row);
+  }
+  console.log(row);
 }
