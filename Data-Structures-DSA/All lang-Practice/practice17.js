69f8f30a5fd64b6177bd3553b37f29cedaf8c9fd
Sahil Golu <sahilkilaniya1@gmail.com>
2026-08-03 11:05:05 +0530

    commit

--- Data-Structures-DSA/All lang-Practice/practice17.js
+++ Data-Structures-DSA/All lang-Practice/practice17.js
@@ -38,18 +38,18 @@
 //     console.log(row);
 // }s
 
-let n = 5;
-for (let i = 1; i <= n; i++) {
-  let row = "";
-  for (let j = 1; j <= n; j++) {
-    if (i === j || i + j === n + 1) {
-      row += "*";
-    } else {
-      row += " ";
-    }
-  }
-  console.log(row);
-}
+// let n = 5;
+// for (let i = 1; i <= n; i++) {
+//   let row = "";
+//   for (let j = 1; j <= n; j++) {
+//     if (i === j || i + j === n + 1) {
+//       row += "*";
+//     } else {
+//       row += " ";
+//     }
+  // }
+  // console.log(row);
+// }
 
 // let n =5;
 // for(let i=1; i<=n; i++){
--- Data-Structures-DSA/All lang-Practice/tempCodeRunnerFile.js
+++ Data-Structures-DSA/All lang-Practice/tempCodeRunnerFile.js
@@ -1,3 +1,12 @@
-  for(let s=1; s<=n-i; s++){
-      row+="*";
-  }
+let n =5;
+for(let i=1; i<=n; i++){
+    let row ='';
+    for(let j =1; j<=i; j++){
+        if(i==j){
+            row+="*";
+        }else{
+            row+=" ";
+        }
+    }
+    console.log(row);
+}
