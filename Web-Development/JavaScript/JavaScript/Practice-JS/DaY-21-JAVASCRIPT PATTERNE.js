dc4375f54628ed87a60c0d64a502df73173ad466
Sahil Golu <sahilkilaniya1@gmail.com>
2026-08-03 12:25:31 +0530

    COMMIT

---
+++ Web-Development/JavaScript/JavaScript/Practice-JS/DaY-21-JAVASCRIPT PATTERNE.js
@@ -0,0 +1,106 @@
+// let n = 3;
+// for (let i = 1; i <= n; i++) {
+//   let row = "";
+
+//   for (let j = 1; j <= i; j++) {
+//     row += "*";
+//   }
+//   for(let s=1; s<=n*2+1; s++){
+//       row+="*";
+//   }
+//   console.log(row);
+// }
+
+// let row = 3;
+// let coln = 7;
+// for(let i=1; i<=3; i++){
+//     let row ="";
+
+//     for(let j=1; j<=7; j++){
+//         row += "*";
+//     }
+//     console.log(row);
+// }
+
+// let n = 5;
+// for (let i = 1; i <= n; i++) {
+//   let row = "";
+
+//   for (let j = 1; j <= i; j++) {
+//     if (i == 1 || j == 1 || (i == 2 && j == 2) || (i == 4 && j == 4)) {
+//       row += "*";
+//     } else {
+//       row += "";
+//     }
+//   }
+//   console.log(row);
+// }
+
+// let  n = 5;
+// for(let i=n; i>=1; i--){
+//     let row ="";
+//     for(let j=1; j<=i; j++){
+//         row+="*";
+//     }
+//     console.log(row);
+// }
+
+// let n = 5;
+// for (let i = 1; i <= n; i++) {
+//   let row = "";
+//   for (let j = 1; j <= i-1; j++) {
+//     row += " ";
+//   }
+//   for(let s=1; s<=n-i+1; s++){
+//     row+="*";
+//   }
+//   console.log(row);
+// }
+
+// let n = 5;
+// for(let i=1; i<=n; i++){
+//     let row ="";
+//     for(let j=1; j<=n-i; j++){
+//         row+=" ";
+//     }
+//     for(let s=1; s<=i; s++){
+//         row+="*";
+//     }
+//     console.log(row);
+// }
+
+// let n =5;
+// for(let i=1; i<=n; i++){
+//     let row="";
+//     for(let j=1; j<=i; j++){
+//         row+=" ";
+//     }
+//     for(let s=1; s<=n; s++){
+//         row+="*";
+//     }
+//     console.log(row);
+// }
+
+// let n =5;
+// for(let i=1; i<=n; i++){
+//     let row="";
+//     for(let j=1; j<=n-i; j++){
+//         row+=" ";
+//     }
+//     for(let s=1; s<=n; s++){
+//         row+="*";
+//     }
+//     console.log(row);
+// }
+
+let n = 3;
+for (let i = 1; i <= n; i++) {
+  let row = "";
+  for (let j = 1; j <= n - i; j++) {
+    row += "  ";
+  }
+  for (let k = 1; k <= i; k++) {
+    row += "* ";
+  }
+  console.log(row);
+}
