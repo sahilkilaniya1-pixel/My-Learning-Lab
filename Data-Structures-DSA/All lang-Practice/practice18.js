f8c2328ddfd68a57d65b9055d1e3a14e32dfaea3
Sahil Golu <sahilkilaniya1@gmail.com>
2026-08-03 11:28:34 +0530

    commit

--- Data-Structures-DSA/All lang-Practice/practice18.js
+++ Data-Structures-DSA/All lang-Practice/practice18.js
@@ -22,9 +22,6 @@
 //     console.log(row);
 // }
 
-
-
-
 // let n = 5;
 // for (let i = 1; i <= n; i++) {
 //   let row = "";
@@ -39,8 +36,6 @@
 //   console.log(row);
 // }
 
-
-
 // let  n = 5;
 // for(let i=n; i>=1; i--){
 //     let row ="";
@@ -50,16 +45,11 @@
 //     console.log(row);
 // }
 
-
-
-
-
-
-let  n = 5;
-for(let i=n; i<=n; i++){
-    let row =" ";
-    for(let j=1; j<=n; j++){
-        row+="*";
-    }
-    console.log(row);
+let n = 5;
+for (let i = n; i <= n; i++) {
+  let row = " ";
+  for (let j = 1; j <= n; j++) {
+    row += "*";
+  }
+  console.log(row);
 }
