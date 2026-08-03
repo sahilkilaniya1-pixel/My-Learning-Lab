b542ca3bb87f1cbffe592e9ad6db866c5d355e9e
Sahil Golu <sahilkilaniya1@gmail.com>
2026-08-03 14:39:31 +0530

    commit

--- Web-Development/JavaScript/JavaScript/Practice-JS/DaY-21-JAVASCRIPT PATTERN.js
+++ Web-Development/JavaScript/JavaScript/Practice-JS/DaY-21-JAVASCRIPT PATTERN.js
@@ -93,14 +93,14 @@
 //     console.log(row);
 // }
 
-let n = 3;
-for (let i = 1; i <= n; i++) {
-  let row = "";
-  for (let j = 1; j <= n - i; j++) {
-    row += "  ";
-  }
-  for (let k = 1; k <= i; k++) {
-    row += "* ";
-  }
-  console.log(row);
-}
+// let n = 3;
+// for (let i = 1; i <= n; i++) {
+//   let row = "";
+//   for (let j = 1; j <= n - i; j++) {
+//     row += "  ";
+//   }
+//   for (let k = 1; k <= i; k++) {
+//     row += "* ";
+//   }
+//   console.log(row);
+// }
