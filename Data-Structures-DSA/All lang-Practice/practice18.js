4adbffb55073b4a3daff2d3a898b1e1278ff17be
Sahil Golu <sahilkilaniya1@gmail.com>
2026-08-03 10:29:57 +0530

    commit

--- Data-Structures-DSA/All lang-Practice/practice18.js
+++ Data-Structures-DSA/All lang-Practice/practice18.js
@@ -1,9 +1,12 @@
-let n = 5;
-for (let i=1; i<=i; i++){
-    let row = "";
+let n = 3;
+for (let i = 1; i <= n; i++) {
+  let row = "";
 
-    for(let j=1; j<=i; j++){
-        row+="*";
-    }
-    console.log(row);
+  for (let j = 1; j <= i; j++) {
+    row += "*";
+  }
+  for(let s=1; s<=n*2; s++){
+      row+="*";
+  }
+  console.log(row);
 }
--- Data-Structures-DSA/All lang-Practice/tempCodeRunnerFile.js
+++ Data-Structures-DSA/All lang-Practice/tempCodeRunnerFile.js
@@ -1,16 +1,3 @@
-let n = 5;
-for (let i = 1; i <= n; i++) {
-  let row = "";
-
-  for (let j = 1; j <= n - i; j++) {
-    row += " ";
+  for(let s=1; s<=n-i; s++){
+      row+="*";
   }
-  for(let k=1; k<=(2*i-1); k++){
-    if(k==1|| k==(2*i-1)||i==n){
-        row+="*";
-    }else{
-        row+=" ";
-    }
-  }
-  console.log(row);
-}
