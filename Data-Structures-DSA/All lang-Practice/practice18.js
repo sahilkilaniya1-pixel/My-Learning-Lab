dd67fc1c9ebc56f005979983136b49119c72497e
Sahil Golu <sahilkilaniya1@gmail.com>
2026-08-04 00:14:48 +0530

    commit

--- Data-Structures-DSA/All lang-Practice/practice18.js
+++ Data-Structures-DSA/All lang-Practice/practice18.js
@@ -152,3 +152,19 @@
 // console.log("1st Min:", firstMin);
 
 
+function findMaxMin(arr) {
+    if (arr.length === 0) return "Array is empty";
+
+    let max = arr[0];
+    let min = arr[0];
+    for (let i = 1; i < arr.length; i++) {
+        if (arr[i] > max) {
+            max = arr[i];
+        }
+        if (arr[i] < min) {
+            min = arr[i];
+        }
+    }
+
+    return { maximum: max, minimum: min };
+}
