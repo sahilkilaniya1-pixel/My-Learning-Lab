ae3fb7ab4adca95e30d490842eeb358ecba3f97d
Sahil Golu <sahilkilaniya1@gmail.com>
2026-08-06 09:30:35 +0530

    commit

---
+++ Web-Development/JavaScript/JavaScript/Practice-JS/02-arrays-and-strings/Day-26-ARRAY QUESTION.js
@@ -0,0 +1,25 @@
+// Question 1: Linear Search & Index Return
+// let arr = [4, 2, 8, 10, 5];
+// let arr = [4,2,8,10,5];
+// let target = 5;
+// let foundIndex = -1;
+// for (let i = 1; i < arr.length; i++) {
+//   if (arr[i] === target) {
+//     foundIndex = i;
+//     break;
+//   }
+// }
+// console.log(foundIndex);
+
+
+
+
+// Question 2: Count Frequency of Elements (Sorted Array)
+// let arr = [1, 1, 1, 2, 2, 3, 4, 4, 4, 4];
+// let count =0;
+// for(let i=0; i<arr.length; i++){
+//     if(arr[i]!==arr[i+1]){
+//         count++
+//     }
+// }
+// console.log(count);
