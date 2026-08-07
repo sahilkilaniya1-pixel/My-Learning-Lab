cfef0eefc79bf18181e5045f8f2d55b4aa53288e
Sahil Golu <sahilkilaniya1@gmail.com>
2026-08-07 18:49:54 +0530

    commit

--- Web-Development/JavaScript/JavaScript/Practice-JS/02-arrays-and-strings/Geekfor geeks/Basic.js
+++ Web-Development/JavaScript/JavaScript/Practice-JS/02-arrays-and-strings/Geekfor geeks/Basic.js
@@ -1,3 +1,4 @@
+// Question 1:
 /*Given an array, arr[] of n integers, and an integer element x, find whether element x is present in the array. Return the index of the first occurrence of x in the array, or -1 if it doesn't exist.
 Examples:
 Input: arr[] = [1, 2, 3, 4], x = 3
@@ -15,6 +16,8 @@ class Solution {
     return index;
   }
 }
+
+// Question 2:
 /* Given an array arr[]. The task is to find the largest element and return it.
 Examples:
 Input: arr[] = [1, 8, 7, 56, 90]
@@ -34,6 +37,7 @@ class Solution {
   }
 }
 
+// Question 3:
 /*Given two arrays a[] and b[], your task is to determine whether b[] is a subset of a[].
 Examples:
 Input: a[] = [11, 7, 1, 13, 21, 3, 7, 3], b[] = [11, 3, 7, 1, 7]
@@ -59,10 +63,10 @@ class solution {
     return j === b.length;
   }
 }
+
+// Question 4:
 /*Given an array arr[]. Your task is to find the minimum and maximum elements in the array.
-
 Examples:
-
 Input: arr[] = [1, 4, 3, 5, 8, 6]
 Output: [1, 8]
 Explanation: minimum and maximum elements of array are 1 and 8.
@@ -90,6 +94,7 @@ class Solution {
   }
 }
 
+// Question 5:
 /*Given an array arr, rotate the array by one position in clockwise direction.
 
 Examples:
@@ -109,5 +114,27 @@ class Solution {
       arr[i] = arr[i - 1];
     }
     arr[0] = last;
+  }
+}
+
+// Question 7:
+/*Given an integer array arr[], return the sum of all elements of arr.
+
+Examples:
+
+Input: arr[] = [1, 2, 3, 4]
+Output: 10
+Explanation: 1 + 2 + 3 + 4 = 10.
+Input: arr[] = [1, 3, 3]
+Output: 7
+Explanation: 1 + 3 + 3 = 7.
+ */
+class Solution {
+  arraySum(arr) {
+    let sum = 0;
+    for (let i = 0; i < arr.length; i++) {
+      sum = sum + arr[i];
+    }
+    return sum;
   }
 }
