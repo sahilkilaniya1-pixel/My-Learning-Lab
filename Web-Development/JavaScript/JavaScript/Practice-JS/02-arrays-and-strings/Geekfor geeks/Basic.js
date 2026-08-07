fb1451753929c203ca79666a9447ff7ecf8aa396
Sahil Golu <sahilkilaniya1@gmail.com>
2026-08-07 19:08:22 +0530

    commit

--- Web-Development/JavaScript/JavaScript/Practice-JS/02-arrays-and-strings/Geekfor geeks/Basic.js
+++ Web-Development/JavaScript/JavaScript/Practice-JS/02-arrays-and-strings/Geekfor geeks/Basic.js
@@ -219,3 +219,13 @@ class Solution {
     return true;
   }
 }
+
+// Question 11:
+/*Given an unsorted array arr[]. Find the count of elements less than or equal to the given element x.
+
+Examples:
+
+Input: x = 9, arr[] = [10, 1, 2, 8, 4, 5] 
+Output: 5
+Explanation: The 5 elements are 1, 2, 8, 4 and 5.
+ */
