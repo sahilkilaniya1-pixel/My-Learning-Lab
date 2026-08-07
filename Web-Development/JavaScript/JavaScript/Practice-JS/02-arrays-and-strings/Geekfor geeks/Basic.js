6c099d25edb53fe0ddbc22d7245150abcdc656aa
Sahil Golu <sahilkilaniya1@gmail.com>
2026-08-07 23:45:07 +0530

    commit

--- Web-Development/JavaScript/JavaScript/Practice-JS/02-arrays-and-strings/Geekfor geeks/Basic.js
+++ Web-Development/JavaScript/JavaScript/Practice-JS/02-arrays-and-strings/Geekfor geeks/Basic.js
@@ -414,3 +414,28 @@ class Solution {
     return true;
   }
 }
+
+// Question 20:
+/*Given a sorted array arr[] and a value target, return an array of size 2. The first value is the number of elements less than or equal to the target, and the second value is the number of elements greater than or equal to the target.
+
+Examples:
+
+Input: arr[] = [1, 2, 8, 10, 11, 12, 19], target = 0
+Output: [0, 7]
+Explanation: There are no elements less or equal to 0 and 7 elements greater to 0. */
+class Solution {
+  getMoreAndLess(arr, target) {
+    let lessOrEqual = 0;
+    let greaterOrEqual = 0;
+
+    for (let i = 0; i < arr.length; i++) {
+      if (arr[i] <= target) {
+        lessOrEqual++;
+      }
+      if (arr[i] >= target) {
+        greaterOrEqual++;
+      }
+    }
+    return [lessOrEqual, greaterOrEqual];
+  }
+}
