c9a269351ae5bbbedfd12f504ccabaccaccde134
Sahil Golu <sahilkilaniya1@gmail.com>
2026-08-07 18:46:39 +0530

    commit

--- Web-Development/JavaScript/JavaScript/Practice-JS/02-arrays-and-strings/Geekfor geeks/Basic.js
+++ Web-Development/JavaScript/JavaScript/Practice-JS/02-arrays-and-strings/Geekfor geeks/Basic.js
@@ -89,3 +89,25 @@ class Solution {
     return [min, max];
   }
 }
+
+/*Given an array arr, rotate the array by one position in clockwise direction.
+
+Examples:
+
+Input: arr[] = [1, 2, 3, 4, 5]
+Output: [5, 1, 2, 3, 4]
+Explanation: If we rotate arr by one position in clockwise 5 come to the front and remaining those are shifted to the end.
+Input: arr[] = [9, 8, 7, 6, 4, 2, 1, 3]
+Output: [3, 9, 8, 7, 6, 4, 2, 1]
+Explanation: After rotating clock-wise 3 comes in first position.
+ */
+class Solution {
+  rotate(arr) {
+    if (arr.length <= 1) return;
+    let last = arr[arr.length - 1];
+    for (let i = arr.length - 1; i > 0; i--) {
+      arr[i] = arr[i - 1];
+    }
+    arr[0] = last;
+  }
+}
