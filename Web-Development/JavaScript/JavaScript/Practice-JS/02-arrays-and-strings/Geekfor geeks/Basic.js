51f4e738bd3ff89a7101c0b5ac77b5923b405d13
Sahil Golu <sahilkilaniya1@gmail.com>
2026-08-07 19:10:15 +0530

    commit

--- Web-Development/JavaScript/JavaScript/Practice-JS/02-arrays-and-strings/Geekfor geeks/Basic.js
+++ Web-Development/JavaScript/JavaScript/Practice-JS/02-arrays-and-strings/Geekfor geeks/Basic.js
@@ -263,3 +263,13 @@ class Solution {
     }
   }
 }
+
+//Question 13:
+/*You are given an integer n. You need to convert all zeroes of n to 5.
+
+Examples:
+
+Input: n = 1004
+Output: 1554
+Explanation: There are two zeroes in 1004 on replacing all zeroes with 5, the new number will be 1554.
+ */
