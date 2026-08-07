b91518f6282a36f9c089636f5f03f019ec69bd94
Sahil Golu <sahilkilaniya1@gmail.com>
2026-08-07 18:34:38 +0530

    commit

--- Web-Development/JavaScript/JavaScript/Practice-JS/02-arrays-and-strings/Geekfor geeks/Basic.js
+++ Web-Development/JavaScript/JavaScript/Practice-JS/02-arrays-and-strings/Geekfor geeks/Basic.js
@@ -15,3 +15,9 @@ class Solution {
         return index; 
     }
 }
+/* Given an array arr[]. The task is to find the largest element and return it.
+Examples:
+Input: arr[] = [1, 8, 7, 56, 90]
+Output: 90
+Explanation: The largest element of the given array is 90.
+ */
