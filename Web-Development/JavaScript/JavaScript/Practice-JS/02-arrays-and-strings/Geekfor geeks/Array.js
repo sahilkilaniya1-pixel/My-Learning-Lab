bf949a10e102f52ce87a7cdcd02e05c04f8fdffa
Sahil Golu <sahilkilaniya1@gmail.com>
2026-08-07 09:07:22 +0530

    commit

--- Web-Development/JavaScript/JavaScript/Practice-JS/02-arrays-and-strings/Geekfor geeks/Array.js
+++ Web-Development/JavaScript/JavaScript/Practice-JS/02-arrays-and-strings/Geekfor geeks/Array.js
@@ -74,3 +74,23 @@
 //     console.log("not a subset")
 // }
 
+
+
+
+
+// class Solution {
+//     getMinMax(arr) {
+//         if (arr.length === 0) return [];
+//         let min = arr[0];
+//         let max = arr[0];
+//         for (let i = 1; i < arr.length; i++) {
+//             if (arr[i] < min) {
+//                 min = arr[i];
+//             }
+//             if (arr[i] > max) {
+//                 max = arr[i];
+//             }
+//         }
+//         return [min, max];
+//     }
+// }
