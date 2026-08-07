1ee0b09ebb0f9b5bd275470ced9e28636f039e28
Sahil Golu <sahilkilaniya1@gmail.com>
2026-08-07 18:43:57 +0530

    commit

--- Web-Development/JavaScript/JavaScript/Practice-JS/02-arrays-and-strings/Geekfor geeks/Basic.js
+++ Web-Development/JavaScript/JavaScript/Practice-JS/02-arrays-and-strings/Geekfor geeks/Basic.js
@@ -52,11 +52,40 @@ class solution {
         j++;
       } else if (a[i] < b[j]) {
         i++;
-      }
-      else{
+      } else {
         return false;
       }
     }
-    return j===b.length;
+    return j === b.length;
+  }
+}
+/*Given an array arr[]. Your task is to find the minimum and maximum elements in the array.
+
+Examples:
+
+Input: arr[] = [1, 4, 3, 5, 8, 6]
+Output: [1, 8]
+Explanation: minimum and maximum elements of array are 1 and 8.
+Input: arr[] = [12, 3, 15, 7, 9]
+Output: [3, 15]
+Explanation: minimum and maximum element of array are 3 and 15.
+ */
+
+class Solution {
+  getMinMax(arr) {
+    if (arr.length === 0) return [];
+
+    let min = arr[0];
+    let max = arr[0];
+
+    for (let i = 1; i < arr.length; i++) {
+      if (arr[i] < min) {
+        min = arr[i];
+      }
+      if (arr[i] > max) {
+        max = arr[i];
+      }
+    }
+    return [min, max];
   }
 }
