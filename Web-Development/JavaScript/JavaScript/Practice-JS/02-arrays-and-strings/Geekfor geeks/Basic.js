ef329f76aa7292e52bd5b7e0892433dbc9a55847
Sahil Golu <sahilkilaniya1@gmail.com>
2026-08-07 20:05:55 +0530

    commit

--- Web-Development/JavaScript/JavaScript/Practice-JS/02-arrays-and-strings/Geekfor geeks/Basic.js
+++ Web-Development/JavaScript/JavaScript/Practice-JS/02-arrays-and-strings/Geekfor geeks/Basic.js
@@ -294,3 +294,15 @@ class Solution {
     return result;
   }
 }
+
+// Question 14:
+/*Given an array arr[] of positive integers. The task is to return the count of the number of odd and even elements in the array.
+
+Note: Return two elements where the first one in the count of odd & second one is the count of even.
+
+Examples:
+
+Input: arr[] = [1, 2, 3, 4, 5]
+Output: 3 2
+Explanation: There are 3 odd elements (1, 3, 5) and 2 even elements (2 and 4).
+ */
