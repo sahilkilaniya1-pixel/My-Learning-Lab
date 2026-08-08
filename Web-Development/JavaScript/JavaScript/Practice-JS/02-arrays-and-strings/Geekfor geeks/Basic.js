dda6f3ca2f4baeb0d41de37c5054fbe1e96c07da
Sahil Golu <sahilkilaniya1@gmail.com>
2026-08-08 11:53:16 +0530

    commit

--- Web-Development/JavaScript/JavaScript/Practice-JS/02-arrays-and-strings/Geekfor geeks/Basic.js
+++ Web-Development/JavaScript/JavaScript/Practice-JS/02-arrays-and-strings/Geekfor geeks/Basic.js
@@ -493,8 +493,5 @@ output= 1
 Explanation: 
 Players always pick the longer coin from the two ends.
 pick 9, remaining array: [5,3,1,6]
-pick 6, remaining array: [5,3,1]
-pick 5, remaining array: [3,1]
-pick 3, remaining array: [1]
-Final output:1
+
 */
