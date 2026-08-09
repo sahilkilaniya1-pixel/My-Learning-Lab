454f00033d121aea76b6f1fecea2ae59dad9226a
Sahil Golu <sahilkilaniya1@gmail.com>
2026-08-09 09:28:10 +0530

    commit

--- Web-Development/JavaScript/JavaScript/Practice-JS/02-arrays-and-strings/Geekfor geeks/Basic.js
+++ Web-Development/JavaScript/JavaScript/Practice-JS/02-arrays-and-strings/Geekfor geeks/Basic.js
@@ -551,3 +551,14 @@ class Solution {
     return arr;
   }
 }
+
+// Question 24: - Rotate Array
+// Given an array arr[]. Rotate the array to the left (counter-clockwise direction) by d steps, where d is a positive integer. Do the mentioned change in the array in place.
+/*
+Note: Consider the array as circular.
+Examples :
+Input: arr[] = [1, 2, 3, 4, 5], d = 2
+Output: [3, 4, 5, 1, 2]
+Explanation: when rotated by 2 elements, it becomes [3, 4, 5, 1, 2].
+*/
+
