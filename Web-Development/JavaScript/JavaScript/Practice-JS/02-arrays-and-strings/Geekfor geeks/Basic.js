41bffc13a46d0e93e1f58138e571559d76590698
Sahil Golu <sahilkilaniya1@gmail.com>
2026-08-09 09:00:05 +0530

    commit

--- Web-Development/JavaScript/JavaScript/Practice-JS/02-arrays-and-strings/Geekfor geeks/Basic.js
+++ Web-Development/JavaScript/JavaScript/Practice-JS/02-arrays-and-strings/Geekfor geeks/Basic.js
@@ -540,4 +540,7 @@ class Solution {
 
 
 //Question 23- Given an array arr that is not completely filled and a value val, you have to insert the value at the end of the array.
-/* */
+/* Examples :
+Input: arr[] = [1, 2, 3, 4, 5], val = 90
+
+*/
