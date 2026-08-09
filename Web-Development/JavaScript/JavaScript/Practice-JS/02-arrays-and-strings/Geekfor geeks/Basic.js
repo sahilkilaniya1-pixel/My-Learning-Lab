d2258f77cfb23da08511d9c411a1636936e390be
Sahil Golu <sahilkilaniya1@gmail.com>
2026-08-09 09:28:18 +0530

    commit

--- Web-Development/JavaScript/JavaScript/Practice-JS/02-arrays-and-strings/Geekfor geeks/Basic.js
+++ Web-Development/JavaScript/JavaScript/Practice-JS/02-arrays-and-strings/Geekfor geeks/Basic.js
@@ -557,8 +557,6 @@ class Solution {
 /*
 Note: Consider the array as circular.
 Examples :
-Input: arr[] = [1, 2, 3, 4, 5], d = 2
-Output: [3, 4, 5, 1, 2]
-Explanation: when rotated by 2 elements, it becomes [3, 4, 5, 1, 2].
+
 */
 
