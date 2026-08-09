6a372106ae571b48c749be1a6be25c4ffbbcc570
Sahil Golu <sahilkilaniya1@gmail.com>
2026-08-09 08:46:21 +0530

    commit

--- Web-Development/JavaScript/JavaScript/Practice-JS/02-arrays-and-strings/Geekfor geeks/Basic.js
+++ Web-Development/JavaScript/JavaScript/Practice-JS/02-arrays-and-strings/Geekfor geeks/Basic.js
@@ -523,5 +523,6 @@ Note: 1-based indexing is followed.
 Examples :
 Input: arr[] = [1, 2, 3, 4, 5, 6, 7, 8], k = 3
 Output: [1, 2, 6, 4, 5, 3, 7, 8]
+Explanation: 3rd element from beginning is 3 and 3rd element from end is 6, so we replace 3 & 6.
 
  */
