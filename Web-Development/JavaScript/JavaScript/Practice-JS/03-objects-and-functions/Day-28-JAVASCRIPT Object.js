8bc9a64b7f048e80f713be788749e9f667909013
Sahil Golu <sahilkilaniya1@gmail.com>
2026-08-10 23:30:11 +0530

    commit

--- Web-Development/JavaScript/JavaScript/Practice-JS/03-objects-and-functions/Day-28-JAVASCRIPT Object.js
+++ Web-Development/JavaScript/JavaScript/Practice-JS/03-objects-and-functions/Day-28-JAVASCRIPT Object.js
@@ -27,3 +27,4 @@ console.log(combined);   //{ a: 1, b: 2, c: 3 }
 // Shallow Copy vs Deep Copy
 const original = {name:"Sahil", address:{city:"Haryana"}};
 // 1:Shallow copy (nested object reference copy hota hain)
+const shallowCopy = {...original};
