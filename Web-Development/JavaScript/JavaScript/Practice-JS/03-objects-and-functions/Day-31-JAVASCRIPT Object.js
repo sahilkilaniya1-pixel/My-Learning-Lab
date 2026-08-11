fd3d60c2dd0e6f4af117aedc8030012bc5253f83
Sahil Golu <sahilkilaniya1@gmail.com>
2026-08-11 09:48:08 +0530

    commit

--- Web-Development/JavaScript/JavaScript/Practice-JS/03-objects-and-functions/Day-31-JAVASCRIPT Object.js
+++ Web-Development/JavaScript/JavaScript/Practice-JS/03-objects-and-functions/Day-31-JAVASCRIPT Object.js
@@ -1,1 +1,4 @@
 // Pillar 3: Getters & Setters (Data Encapsulation)
+/* 
+Getters (get ) aur setters (set) Pseudo- Properties banate hain. Inka matlab hai ki bahar se ye normal keys (obj.age)jaiso
+*/
