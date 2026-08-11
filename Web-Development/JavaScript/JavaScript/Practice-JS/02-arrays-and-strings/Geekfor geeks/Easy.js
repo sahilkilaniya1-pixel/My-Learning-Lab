301a787ddc443de6798370498d57a8279a2a494d
Sahil Golu <sahilkilaniya1@gmail.com>
2026-08-11 14:38:57 +0530

    commit

---
+++ Web-Development/JavaScript/JavaScript/Practice-JS/02-arrays-and-strings/Geekfor geeks/Easy.js
@@ -0,0 +1,19 @@
+// You are given an array arr[] of size n - 1 that contains distinct integers in the range from 1 to n (inclusive). This array represents a permutation of the integers from 1 to n with one element missing. Your task is to identify and return the missing element.
+
+// Examples:
+
+// Input: arr[] = [1, 2, 3, 5]
+// Output: 4
+// Explanation: All the numbers from 1 to 5 are present except 4.
+class Solution {
+  missingNum(arr) {
+    let n = arr.length + 1;
+    let sum = (n * (n + 1)) / 2;
+    let arrsum = 0;
+    for (let i = 0; i < arr.length; i++) {
+      arrsum += arr[i];
+    }
+    arrsum = sum - arrsum;
+    return arrsum;
+  }
+}
