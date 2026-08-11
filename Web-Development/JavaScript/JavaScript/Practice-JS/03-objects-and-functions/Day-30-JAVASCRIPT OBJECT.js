3e2fcd2325aee1a2c718b146a5b0c1bf364f78c2
Sahil Golu <sahilkilaniya1@gmail.com>
2026-08-11 09:14:12 +0530

    commit

--- Web-Development/JavaScript/JavaScript/Practice-JS/03-objects-and-functions/Day-30-JAVASCRIPT OBJECT.js
+++ Web-Development/JavaScript/JavaScript/Practice-JS/03-objects-and-functions/Day-30-JAVASCRIPT OBJECT.js
@@ -3,4 +3,6 @@
 /*Javascript me Classes real classes nahi hoti (jaise c++ ya java mein hoti hain).Js pure Protoypal Inhertance par kaam karti hai.
 
 Prototype hota kya hai?
+har js object ke pass ek internal link hota hai jisee [[prototype]] bolte hain (Developer Tools me ye __proto__ ke naam s dikhta hai.).
+
 */
