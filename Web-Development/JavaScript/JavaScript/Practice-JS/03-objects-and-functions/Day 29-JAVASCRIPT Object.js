2504a063a3b0de89d558b1a30fb11f0892048c10
Sahil Golu <sahilkilaniya1@gmail.com>
2026-08-11 08:35:29 +0530

    commiit

--- Web-Development/JavaScript/JavaScript/Practice-JS/03-objects-and-functions/Day 29-JAVASCRIPT Object.js
+++ Web-Development/JavaScript/JavaScript/Practice-JS/03-objects-and-functions/Day 29-JAVASCRIPT Object.js
@@ -7,3 +7,11 @@ writable: Value change ho sakti hai ya nahi.
 enumerable: Loops (for...in) me show hoga ya nahi.
 configurable: Delete ya settings change ho sakti hain ya nahi.
 */
+
+const person = {};
+Object.defineProperties(person,"id",{
+    valee: 101,
+    writeable: false,
+    enumerable:false,
+    
+})
