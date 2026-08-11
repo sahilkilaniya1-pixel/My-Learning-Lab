468f9f2d709cbc502ec5bc1493aa46c5a4003d44
Sahil Golu <sahilkilaniya1@gmail.com>
2026-08-11 14:39:55 +0530

    commit

--- Web-Development/JavaScript/JavaScript/Practice-JS/02-arrays-and-strings/Geekfor geeks/Easy.js
+++ Web-Development/JavaScript/JavaScript/Practice-JS/02-arrays-and-strings/Geekfor geeks/Easy.js
@@ -17,3 +17,32 @@ class Solution {
     return arrsum;
   }
 }
+
+// Question 2- Second Largest
+/*
+Given an array of positive integers arr[], return the second largest element from the array. If the second largest element doesn't exist then return -1.
+
+Note: The second largest element should not be equal to the largest element.
+
+Examples:
+
+Input: arr[] = [12, 35, 1, 10, 34, 1]
+Output: 34
+Explanation: The largest element of the array is 35 and the second largest element is 34.
+*/
+class Solution {
+  getSecondLargest(arr) {
+    let largest = -1;
+    let secondlargest = -1;
+    for (let num of arr) {
+      if (num > largest) {
+        secondlargest = largest;
+        largest = num;
+      } else if (num < largest && num > secondlargest) {
+        secondlargest = num;
+      }
+    }
+
+    return secondlargest;
+  }
+}
