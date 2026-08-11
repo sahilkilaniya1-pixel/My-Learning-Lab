9e62c9d33dc63320ea110471298e89baa39041ca
Sahil Golu <sahilkilaniya1@gmail.com>
2026-08-11 11:38:53 +0530

    commit

--- Web-Development/JavaScript/JavaScript/Practice-JS/03-objects-and-functions/Day-32-JAVSCRIPT Object Metaprogramming Question.js
+++ Web-Development/JavaScript/JavaScript/Practice-JS/03-objects-and-functions/Day-32-JAVSCRIPT Object Metaprogramming Question.js
@@ -2,3 +2,8 @@
 /*
 Metaprogramming ka matlab hota hain "Code write karna jo doosre code ko inspect,modify ya intercept kare." Normal JS me jab aap obj.key likhte ho  toh direct value milti hai, Lekin Proxy lagane ke baad aap is behavior ke beech me apna custom logic fit kar sakte ho.
 */
+
+// 1. Proxy Component Architecture
+/* 
+Proxy ke 3
+*/
