12351b925b26cd07313090a636434aa4046d0ca0
Sahil Golu <sahilkilaniya1@gmail.com>
2026-08-11 09:50:22 +0530

    commit

--- Web-Development/JavaScript/JavaScript/Practice-JS/03-objects-and-functions/Day-31-JAVASCRIPT Object.js
+++ Web-Development/JavaScript/JavaScript/Practice-JS/03-objects-and-functions/Day-31-JAVASCRIPT Object.js
@@ -2,3 +2,9 @@
 /* 
 Getters (get ) aur setters (set) Pseudo- Properties banate hain. Inka matlab hai ki bahar se ye normal keys (obj.age)jaisi dikhti hain, lekin background me functions executre hote hain.
 */
+// Dynamic Validation & Computed Properties
+const userAccount = {
+    firstname :"Sahil",
+    lastname: "Kilaniya",
+    _rawBalance: 1000, //'_'standar notation hai private/internal data ke liye
+}
