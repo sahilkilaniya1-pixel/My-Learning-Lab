fa6875dbc5f3b7029caf83b260b45cafbc58acf6
Sahil Golu <sahilkilaniya1@gmail.com>
2026-08-11 11:37:30 +0530

    commit

--- Web-Development/JavaScript/JavaScript/Practice-JS/03-objects-and-functions/Day-32-JAVSCRIPT Object Metaprogramming Question.js
+++ Web-Development/JavaScript/JavaScript/Practice-JS/03-objects-and-functions/Day-32-JAVSCRIPT Object Metaprogramming Question.js
@@ -1,4 +1,4 @@
 // Module 4: Metaprogramming (Proxies & Reflect)
 /*
-Metaprogramming ka matlab hota hain "Code write karna jo doosre code ko inspect,modify ya intercept kare."
+Metaprogramming ka matlab hota hain "Code write karna jo doosre code ko inspect,modify ya intercept kare." Normal JS me jab aap obj.key likhte ho 
 */
