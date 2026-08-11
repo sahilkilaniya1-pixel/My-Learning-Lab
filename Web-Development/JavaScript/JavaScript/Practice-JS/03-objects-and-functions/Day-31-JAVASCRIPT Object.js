cf326be330b0bf4c6acf5b2eb6df4cdbddb6fc98
Sahil Golu <sahilkilaniya1@gmail.com>
2026-08-11 09:48:40 +0530

    commit

--- Web-Development/JavaScript/JavaScript/Practice-JS/03-objects-and-functions/Day-31-JAVASCRIPT Object.js
+++ Web-Development/JavaScript/JavaScript/Practice-JS/03-objects-and-functions/Day-31-JAVASCRIPT Object.js
@@ -1,4 +1,4 @@
 // Pillar 3: Getters & Setters (Data Encapsulation)
 /* 
-Getters (get ) aur setters (set) Pseudo- Properties banate hain. Inka matlab hai ki bahar se ye normal keys (obj.age)jaiso
+Getters (get ) aur setters (set) Pseudo- Properties banate hain. Inka matlab hai ki bahar se ye normal keys (obj.age)jaisi dikhti hain, lekin background me functions executre hote hain.
 */
