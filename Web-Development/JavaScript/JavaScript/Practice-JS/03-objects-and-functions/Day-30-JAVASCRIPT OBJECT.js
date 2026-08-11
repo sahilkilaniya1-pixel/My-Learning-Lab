8340d063cf7c10b68480cdb3dade2d005c0f8151
Sahil Golu <sahilkilaniya1@gmail.com>
2026-08-11 09:13:16 +0530

    commit

--- Web-Development/JavaScript/JavaScript/Practice-JS/03-objects-and-functions/Day-30-JAVASCRIPT OBJECT.js
+++ Web-Development/JavaScript/JavaScript/Practice-JS/03-objects-and-functions/Day-30-JAVASCRIPT OBJECT.js
@@ -1,3 +1,6 @@
 /*Pillar 2: Prototypes & Prototype Chain (Memory Mechanism) */
 
-/*Javascript me Classes real classes nahi hoti (jaise c++ ya java mein hoti hain).Js pure Protoypal Inhertance par kaam karti hai.*/
+/*Javascript me Classes real classes nahi hoti (jaise c++ ya java mein hoti hain).Js pure Protoypal Inhertance par kaam karti hai.
+
+Prototype hota kya hai?
+*/
