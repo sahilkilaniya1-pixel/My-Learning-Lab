433e6dcbff25348d15688cbddb651cb3620dfd35
Sahil Golu <sahilkilaniya1@gmail.com>
2026-08-11 14:40:42 +0530

    commit

--- Web-Development/JavaScript/JavaScript/Practice-JS/02-arrays-and-strings/Geekfor geeks/Easy.js
+++ Web-Development/JavaScript/JavaScript/Practice-JS/02-arrays-and-strings/Geekfor geeks/Easy.js
@@ -1,7 +1,6 @@
+// Question 1- Missing in Array:
 // You are given an array arr[] of size n - 1 that contains distinct integers in the range from 1 to n (inclusive). This array represents a permutation of the integers from 1 to n with one element missing. Your task is to identify and return the missing element.
-
 // Examples:
-
 // Input: arr[] = [1, 2, 3, 5]
 // Output: 4
 // Explanation: All the numbers from 1 to 5 are present except 4.
@@ -18,7 +17,7 @@ class Solution {
   }
 }
 
-// Question 2- Second Largest
+// Question 2- Second Largest:
 /*
 Given an array of positive integers arr[], return the second largest element from the array. If the second largest element doesn't exist then return -1.
 
