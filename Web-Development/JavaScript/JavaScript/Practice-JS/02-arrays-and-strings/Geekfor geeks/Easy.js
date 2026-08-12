3e7d0c363b345e7e52fef1ac03269c75651d16b7
Sahil Golu <sahilkilaniya1@gmail.com>
2026-08-12 13:48:02 +0530

    commit

--- Web-Development/JavaScript/JavaScript/Practice-JS/02-arrays-and-strings/Geekfor geeks/Easy.js
+++ Web-Development/JavaScript/JavaScript/Practice-JS/02-arrays-and-strings/Geekfor geeks/Easy.js
@@ -106,3 +106,17 @@ class Solution {
     return output;
   }
 }
+
+// Question 5: First Occurrence in Sorted
+/*
+Given a sorted array arr[] and an integer k, find the position(0-based indexing) at which k is present in the array using binary search. If k doesn't exist in arr[] return -1. 
+
+Note: If multiple occurrences are there, please return the smallest index.
+
+Examples:
+
+Input: arr[] = [1, 2, 3, 4, 5], k = 4
+Output: 3
+Explanation: 4 appears at index 3.
+
+*/
