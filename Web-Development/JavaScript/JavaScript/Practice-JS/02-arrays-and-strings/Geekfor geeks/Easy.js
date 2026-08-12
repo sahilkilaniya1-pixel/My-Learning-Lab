a57c1fc11a934548e7d67b3a2f538668cbfaf370
Sahil Golu <sahilkilaniya1@gmail.com>
2026-08-12 13:28:56 +0530

    commit

--- Web-Development/JavaScript/JavaScript/Practice-JS/02-arrays-and-strings/Geekfor geeks/Easy.js
+++ Web-Development/JavaScript/JavaScript/Practice-JS/02-arrays-and-strings/Geekfor geeks/Easy.js
@@ -45,3 +45,31 @@ class Solution {
     return secondlargest;
   }
 }
+
+// Question 3: Array Leaders
+/*
+You are given an array arr of positive integers. Your task is to find all the leaders in the array. An element is considered a leader if it is greater than or equal to all elements to its right. The rightmost element is always a leader.
+
+Examples:
+
+Input: arr = [16, 17, 4, 3, 5, 2]
+Output: [17, 5, 2]
+Explanation: Note that there is nothing greater on the right side of 17, 5 and, 2.
+
+*/
+
+class Solution {
+  leaders(a) {
+    let n = a.length;
+    let result = [];
+    let maxFromRight = a[n - 1];
+    result.push(maxFromRight);
+    for (let i = n - 2; i >= 0; i--) {
+      if (a[i] >= maxFromRight) {
+        result.push(a[i]);
+        maxFromRight = a[i];
+      }
+    }
+    return result.reverse();
+  }
+}
