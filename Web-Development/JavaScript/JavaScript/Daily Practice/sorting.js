9ba1b39e6c67cc7ebe443f97baa3b9f2c8333cc7
Sahil Golu <sahilkilaniya1@gmail.com>
2026-08-13 16:15:11 +0530

    commit

--- Web-Development/JavaScript/JavaScript/Daily Practice/sorting.js
+++ Web-Development/JavaScript/JavaScript/Daily Practice/sorting.js
@@ -1,8 +1,9 @@
-let a = [1, 4, 3, 2, 5, 7, 6];
+// let a = [1, 4, 3, 2, 5, 7, 6];
+let a = [1,6,3,8,7,]
 export function sort_array(arr) {
     for (let i = 0; i < arr.length -1 ; i++) {
         for (let j = 0; j < arr.length; j++) {
-            if (arr[j > arr[j + 1]) {
+            if (arr[j] > arr[j + 1]) {
                 let temp = arr[j];
                 arr[j] = arr[j + 1];
                 arr[j + 1] = temp;
