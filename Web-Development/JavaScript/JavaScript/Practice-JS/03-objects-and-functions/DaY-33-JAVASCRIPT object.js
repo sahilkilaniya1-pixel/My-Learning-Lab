e0807312162380eaa2d5feb2998babff7cbe93a3
Sahil Golu <sahilkilaniya1@gmail.com>
2026-08-13 19:03:10 +0530

    commit

--- Web-Development/JavaScript/JavaScript/Practice-JS/03-objects-and-functions/DaY-33-JAVASCRIPT object.js
+++ Web-Development/JavaScript/JavaScript/Practice-JS/03-objects-and-functions/DaY-33-JAVASCRIPT object.js
@@ -2,5 +2,5 @@
 /*
 Problem: Ek string given hain. har character kitni baar repeat hua hain, uska object bana kar return karo.
 
-Approch: Loop chalao, agar key pehle se object mein
+Approch: Loop chalao, agar key pehle se object mein hai toh +1 karo, nahi 1 assign karo.
 */
