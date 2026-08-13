67c01674d3e33c2e2d0c49b8c7109e3720c4be8c
Sahil Golu <sahilkilaniya1@gmail.com>
2026-08-13 19:02:51 +0530

    commit

--- Web-Development/JavaScript/JavaScript/Practice-JS/03-objects-and-functions/DaY-33-JAVASCRIPT object.js
+++ Web-Development/JavaScript/JavaScript/Practice-JS/03-objects-and-functions/DaY-33-JAVASCRIPT object.js
@@ -1,4 +1,6 @@
 // Level 1 (Basic): Character Frequency Counter
 /*
 Problem: Ek string given hain. har character kitni baar repeat hua hain, uska object bana kar return karo.
+
+Approch: Loop chalao, agar key pehle se object mein
 */
