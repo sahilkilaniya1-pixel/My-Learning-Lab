148c4577eb7ff6ca5e6a0218cca470b7fbb61e29
Sahil Golu <sahilkilaniya1@gmail.com>
2026-08-13 15:36:37 +0530

    commit

---
+++ Web-Development/JavaScript/JavaScript/Daily Practice/2.js
@@ -0,0 +1,4 @@
+import {sort_array} from './sorting.js'
+let a = [1, 4, 3, 2, 5, 7, 6];
+let n=sort_array(a)
+console.log(n);
---
+++ Web-Development/JavaScript/JavaScript/Daily Practice/sorting.js
@@ -0,0 +1,14 @@
+let a = [1, 4, 3, 2, 5, 7, 6];
+export function sort_array(arr) {
+    for (let i = 0; i < arr.length -1 ; i++) {
+        for (let j = 0; j < arr.length; j++) {
+            if (arr[j > arr[j + 1]) {
+                let temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+            }
+        }
+    }
+    return arr;
+}
+console.log(sort_array(a));
