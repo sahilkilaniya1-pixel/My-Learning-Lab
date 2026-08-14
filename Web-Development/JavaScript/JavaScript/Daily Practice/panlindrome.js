740bcbce43760d2f19434797b148b0a1657a113d
Sahil Golu <sahilkilaniya1@gmail.com>
2026-08-14 06:52:50 +0530

    commit

--- Web-Development/JavaScript/JavaScript/Daily Practice/panlindrome.js
+++ Web-Development/JavaScript/JavaScript/Daily Practice/panlindrome.js
@@ -2,12 +2,12 @@ function palindrome() {
     let cleanStr = str.toLowerCase();
     let left = 0;
     let right = cleanStr.length - 1;
-    while(left<right){
-        if(cleanStr[left]!==cleanStr[right]){
+    while (left < right) {
+        if (cleanStr[left] !== cleanStr[right]) {
             return false;
         }
-        left ++;
-        right --;
+        left++;
+        right--;
     }
     return true;
 }
