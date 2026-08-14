4ca684da70ef5d7f08bf02eecdb155381efc5256
Sahil Golu <sahilkilaniya1@gmail.com>
2026-08-14 06:56:39 +0530

    commit

--- Web-Development/JavaScript/JavaScript/Daily Practice/armstrong.js
+++ Web-Development/JavaScript/JavaScript/Daily Practice/armstrong.js
@@ -1,15 +1,15 @@
-function isArmstrong(num){
-    if(num<0) 
+function isArmstrong(num) {
+    if (num < 0)
         return false;
     let original = num;
     let digitCount = num.toString().length;
     let sum = 0;
-    while(original>0){
-        let lastDigit = original %10;
-        sum+=Math.pow(lastDigit,digitCount);
-        original = Math.floor(original/10);
+    while (original > 0) {
+        let lastDigit = original % 10;
+        sum += Math.pow(lastDigit, digitCount);
+        original = Math.floor(original / 10);
     }
-    return sum===num;
+    return sum === num;
 }
 console.log(isArmstrong(153));
 console.log(isArmstrong(1634));
