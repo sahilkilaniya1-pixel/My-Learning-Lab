58ec08095622ea30fa649975452a169a213977fc
Sahil Golu <sahilkilaniya1@gmail.com>
2026-08-14 07:24:41 +0530

    commit

--- Web-Development/JavaScript/JavaScript/Practice-JS/03-objects-and-functions/DaY-33-JAVASCRIPT object.js
+++ Web-Development/JavaScript/JavaScript/Practice-JS/03-objects-and-functions/DaY-33-JAVASCRIPT object.js
@@ -75,7 +75,4 @@ function calculateTotalSum(obj){
 
         if(typeof value === 'number'){
             total += value;
-        }
-        else if (typeof value ==='object' && value !)
-    }
-}
+        
