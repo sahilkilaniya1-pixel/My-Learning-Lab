cb619a646bc43976dc9c90a15f02c18aa4518b36
Sahil Golu <sahilkilaniya1@gmail.com>
2026-08-14 06:50:03 +0530

    commit

--- Web-Development/JavaScript/JavaScript/Daily Practice/panlindrome.js
+++ Web-Development/JavaScript/JavaScript/Daily Practice/panlindrome.js
@@ -3,6 +3,8 @@ function palindrome() {
     let left = 0;
     let right = cleanStr.length - 1;
     while(left<right){
-        
+        if(cleanStr[left]!==cleanStr[right]){
+            return false;
+        }
     }
 }
