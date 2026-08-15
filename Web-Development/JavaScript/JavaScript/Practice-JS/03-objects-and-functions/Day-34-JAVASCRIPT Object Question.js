27f66c47e86e7252cdec19fa38a5d53bb59cdd9c
Sahil Golu <sahilkilaniya1@gmail.com>
2026-08-15 09:13:32 +0530

    commit

--- Web-Development/JavaScript/JavaScript/Practice-JS/03-objects-and-functions/Day-34-JAVASCRIPT Object Question.js
+++ Web-Development/JavaScript/JavaScript/Practice-JS/03-objects-and-functions/Day-34-JAVASCRIPT Object Question.js
@@ -8,6 +8,9 @@ function flattenObject(obj, parentKey = '',result = {}){
     for(let key in obj){
         if(obj.hasOwnProperty(key)){
             const newKey = parentKey ? `${parentKey}.${key}` : key;
+            if(typeof obj[key]==='object' && obj[key] !== null){
+                
+            }
         }
     }
 }
