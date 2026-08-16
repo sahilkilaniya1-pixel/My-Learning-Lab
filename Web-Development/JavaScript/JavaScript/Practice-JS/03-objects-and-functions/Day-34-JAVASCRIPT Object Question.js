04ff03f04aeb425ea8dc430aee43a6aeed871585
Sahil Golu <sahilkilaniya1@gmail.com>
2026-08-16 17:25:31 +0530

    commit

--- Web-Development/JavaScript/JavaScript/Practice-JS/03-objects-and-functions/Day-34-JAVASCRIPT Object Question.js
+++ Web-Development/JavaScript/JavaScript/Practice-JS/03-objects-and-functions/Day-34-JAVASCRIPT Object Question.js
@@ -1,16 +1,6 @@
 /*
-Question 1: Deep Flatten Object
-Kya aur Kyun Karna Hai?
-Nested object mein keys ke andar keys hoti hain (user.address.city). Flattening ka matlab hai saari internal keys ko ek hi level par laana aur keys ka naam parent path ke sath jodna (jaise address.city).
-*/
+Question 1: Cart Summary Calculator
 
-function flattenObject(obj, parentKey = '',result = {}){
-    for(let key in obj){
-        if(obj.hasOwnProperty(key)){
-            const newKey = parentKey ? `${parentKey}.${key}` : key;
-            if(typeof obj[key]==='object' && obj[key] !== null){
-                
-            }
-        }
-    }
-}
+Human Thought Process (Cashier Mindset)
+
+*/
