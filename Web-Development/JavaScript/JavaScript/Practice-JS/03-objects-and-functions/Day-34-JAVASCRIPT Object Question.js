9507cd960674b934064caaae9115e490673cd5d5
Sahil Golu <sahilkilaniya1@gmail.com>
2026-08-16 17:25:53 +0530

    commit

--- Web-Development/JavaScript/JavaScript/Practice-JS/03-objects-and-functions/Day-34-JAVASCRIPT Object Question.js
+++ Web-Development/JavaScript/JavaScript/Practice-JS/03-objects-and-functions/Day-34-JAVASCRIPT Object Question.js
@@ -3,4 +3,5 @@ Question 1: Cart Summary Calculator
 
 Human Thought Process (Cashier Mindset)
 "Jab main counter par bill banata hoon, toh main ek 
+empty slip lekar baithta hoon jahan total count, 
 */
