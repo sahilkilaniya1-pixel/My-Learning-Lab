50e0d425eda3b70ce9e9f2c5bf5df519ef9ca8fa
Sahil Golu <sahilkilaniya1@gmail.com>
2026-08-16 18:01:32 +0530

    commit

--- Web-Development/JavaScript/JavaScript/Practice-JS/03-objects-and-functions/Day-34-JAVASCRIPT Object Question.js
+++ Web-Development/JavaScript/JavaScript/Practice-JS/03-objects-and-functions/Day-34-JAVASCRIPT Object Question.js
@@ -48,5 +48,6 @@ console.log(calculateChartSummary(cart));
 Question 2: Course - Student Inverter
 Human Thought Process (Notice Board Mindset)
 Phele list hai cource->Students. Mujhe chahiye student -> Cources.
-Main student ka naam boluga, jaise Sahil. pucho ''
+Main student ka naam boluga, jaise Sahil. pucho 'kya sahil meri register notebook mein phele se hai?'
+- 
 */
