5227ed847cb5fb1379f1968020c50169066648b6
Sahil Golu <sahilkilaniya1@gmail.com>
2026-08-16 17:27:13 +0530

    commit

--- Web-Development/JavaScript/JavaScript/Practice-JS/03-objects-and-functions/Day-34-JAVASCRIPT Object Question.js
+++ Web-Development/JavaScript/JavaScript/Practice-JS/03-objects-and-functions/Day-34-JAVASCRIPT Object Question.js
@@ -7,3 +7,6 @@ empty slip lekar baithta hoon jahan total count,
 total price, aur tax add karta jata hoon. Har item 
 par uski category dekhkar tax calculate karta hoon, aur end mein total bill hand-over kar deta hoon." 
 */
+function calculateChartSummary(cart){
+    
+}
