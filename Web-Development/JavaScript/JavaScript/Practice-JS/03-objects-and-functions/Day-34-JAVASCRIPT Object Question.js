1a2d1c7733e46424ad8ae0ab0263380aa01ef169
Sahil Golu <sahilkilaniya1@gmail.com>
2026-08-16 17:26:14 +0530

    commit

--- Web-Development/JavaScript/JavaScript/Practice-JS/03-objects-and-functions/Day-34-JAVASCRIPT Object Question.js
+++ Web-Development/JavaScript/JavaScript/Practice-JS/03-objects-and-functions/Day-34-JAVASCRIPT Object Question.js
@@ -5,4 +5,5 @@ Human Thought Process (Cashier Mindset)
 "Jab main counter par bill banata hoon, toh main ek 
 empty slip lekar baithta hoon jahan total count,
 total price, aur tax add karta jata hoon. Har item 
+par uski category dekhkar tax calculate karta hoon, aur end mein total bill hand-over kar deta hoon." 
 */
