7ade5b303ce85cd6449380f3d1d444f15626304a
Sahil Golu <sahilkilaniya1@gmail.com>
2026-08-17 09:41:36 +0530

    commit

--- Data-Structures-DSA/CPP/C++/Program.cpp
+++ Data-Structures-DSA/CPP/C++/Program.cpp
@@ -14,4 +14,8 @@ std(namespace): C++ mein standard library ki
 cheezein ek "Container" ke andar hoti hain jis namespace kehte hain. std ka matlab "Standard"hai.
 
 
-::(scope resolution operator): yeh compiler ko bata hai ki cout kahan dhoondhna hai (yani std namespace  */
+::(scope resolution operator): yeh compiler ko bata hai ki cout kahan dhoondhna hai (yani std namespace ke andar).
+
+
+
+ */
