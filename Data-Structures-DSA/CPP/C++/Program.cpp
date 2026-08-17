b9b237fad8dbd9b302ac694605bee8bbaf2a0b15
Sahil Golu <sahilkilaniya1@gmail.com>
2026-08-17 09:31:09 +0530

    commit

--- Data-Structures-DSA/CPP/C++/Program.cpp
+++ Data-Structures-DSA/CPP/C++/Program.cpp
@@ -1,7 +1,7 @@
 #include <iostream>  //Header File
 using namespace std; //which nanespace ->standar name space
 
-int main() {
+int main() {  //{} show source of code
     //cout is a keyword  use for printing or output 
     //"" is a string cout print string.
     cout << "Namaste Dunia" << endl; 
