7462fdc5030af9a8778a297eca6aa788442d786b
Sahil Golu <sahilkilaniya1@gmail.com>
2026-08-17 09:24:16 +0530

    commit

--- Data-Structures-DSA/CPP/C++/Program.cpp
+++ Data-Structures-DSA/CPP/C++/Program.cpp
@@ -2,6 +2,7 @@
 using namespace std; //which nanespace ->standar name space
 
 int main() {
+    //cout is a keyword  use for printing or output 
     cout << "Namaste Dunia" << endl;
     return 0;
 }
