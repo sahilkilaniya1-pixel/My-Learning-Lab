2ab3799c7031beeaf56464f47d0ace48ef5b5160
Sahil Golu <sahilkilaniya1@gmail.com>
2026-08-17 09:30:13 +0530

    commit

--- Data-Structures-DSA/CPP/C++/Program.cpp
+++ Data-Structures-DSA/CPP/C++/Program.cpp
@@ -5,5 +5,6 @@ int main() {
     //cout is a keyword  use for printing or output 
     //"" is a string cout print string.
     cout << "Namaste Dunia" << endl; 
+    // <<endl is a keyword for print new line
     return 0;
 }
