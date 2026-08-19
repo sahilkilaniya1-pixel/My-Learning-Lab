9e3f82fa453b9a18c053ff3590e49b50baebd213
Sahil Golu <sahilkilaniya1@gmail.com>
2026-08-19 09:45:17 +0530

    commit

--- Web-Development/JavaScript/JavaScript/Practice-JS/03-objects-and-functions/DaY-35-JAVASCRIPT FUNCTION.js
+++ Web-Development/JavaScript/JavaScript/Practice-JS/03-objects-and-functions/DaY-35-JAVASCRIPT FUNCTION.js
@@ -45,5 +45,5 @@ console.log("Printing Sum:",ans); //Printing Sum: 6
 let fullname = getMyname("Sahil","Kilaniya");
 function getMyname(firstName, lastName){
     let fullName = firstName + " "+ lastName;
-    
-}
+    return fullName;
+};
