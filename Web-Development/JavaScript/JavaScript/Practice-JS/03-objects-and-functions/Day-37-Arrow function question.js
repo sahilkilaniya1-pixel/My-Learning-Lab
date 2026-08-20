efa843e47caeaf88187392d975cc416bf1721ec2
Sahil Golu <sahilkilaniya1@gmail.com>
2026-08-20 15:46:14 +0530

    commit

--- Web-Development/JavaScript/JavaScript/Practice-JS/03-objects-and-functions/Day-37-Arrow function question.js
+++ Web-Development/JavaScript/JavaScript/Practice-JS/03-objects-and-functions/Day-37-Arrow function question.js
@@ -15,6 +15,30 @@ function validateEmail(email) {
 console.log(validateEmail("user@example.com"));
 console.log(validateEmail("invalid.email@"));
 
+// Q2. Write a function validatePassword(password) that returns true only if the password has at least 8 
+// characters, one uppercase letter, one number, and one special character. 
+function validatePassword(password) {
+    if (password.length < 8)
+        return false;
+    let hasUpper = false;
+    let hasNumber = false;
+    let hasSpecial = false;
+    let specials = "!@#$%^&*()_+-=[]{}|;:,.<>?";
+    for (let i = 0; i < password.length; i++) {
+        let char = password[i];
+        if (char >= 'A' && char <= 'Z') hasUpper = true;
+        else if (char >= '0' && char <= '9') hasNumber = true;
+        else {
+            for (let j = 0; j < specials.length; j++) {
+                if (char === specials[j]) hasSpecial = true;
+            }
+        }
+    }
+    return hasUpper && hasNumber && hasSpecial;
+}
+console.log(validatePassword("pass@123"))
+console.log(validatePassword("Pass@123")) 
+
 
 
 // Q3. Write a function validatePhoneNumber(number) that checks if the given string is a valid 10-digit Indian 
@@ -47,33 +71,3 @@ console.log(matchPasswords("Sa123", "Sa12")); //Output-False
 
 
 
-// function showHideElement(elementId) {
-//   let element = document.getElementById(elementId);
-//   if (element.style.display === "none") {
-//     element.style.display = "block";
-//   } else {
-//     element.style.display = "none";
-//   }
-// }
-
-function validatePassword(password) {
-    if (password.length < 8)
-        return false;
-    let hasUpper = false;
-    let hasNumber = false;
-    let hasSpecial = false;
-    let specials = "!@#$%^&*()_+-=[]{}|;:,.<>?";
-    for (let i = 0; i < password.length; i++) {
-        let char = password[i];
-        if (char >= 'A' && char <= 'Z') hasUpper = true;
-        else if (char >= '0' && char <= '9') hasNumber = true;
-        else {
-            for (let j = 0; j < specials.length; j++) {
-                if (char === specials[j]) hasSpecial = true;
-            }
-        }
-    }
-    return hasUpper && hasNumber && hasSpecial;
-}
-console.log(validatePassword("pass@123"))
-console.log(validatePassword("Pass@123")) 
