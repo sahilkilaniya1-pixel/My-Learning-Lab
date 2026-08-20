f90f567108331811a7a860a364ee6f2e926b462d
Sahil Golu <sahilkilaniya1@gmail.com>
2026-08-20 11:04:13 +0530

    commit

--- Web-Development/JavaScript/JavaScript/Practice-JS/03-objects-and-functions/Day-37-Arrow function question.js
+++ Web-Development/JavaScript/JavaScript/Practice-JS/03-objects-and-functions/Day-37-Arrow function question.js
@@ -1,14 +1,4 @@
 function validateEmail(email) {
   let atIndex = -1;
   let dotIndex = -1;
-  for (let i = 0; i < email.length; i++) {
-    if (email[i] === '@') atIndex = i;
-    if (email[i] === '.') dotIndex = i;
-  }
-  if (atIndex < 1 || dotIndex <= atIndex + 1 || dotIndex === email.length - 1) {
-    return false;
-  }
-  return true;
-}
-console.log(validateEmail("user@example.com"));
-console.log(validateEmail("invalid.email@"));
+  
