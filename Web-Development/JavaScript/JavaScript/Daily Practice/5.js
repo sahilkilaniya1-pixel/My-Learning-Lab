c7475b0fec01fd11fb144ae9cc058399bdea9f33
Sahil <sahilkilaniya1@gmail.com>
2026-08-24 22:20:00 +0530

    commit

---
+++ Web-Development/JavaScript/JavaScript/Daily Practice/5.js
@@ -0,0 +1,21 @@
+// const person = {
+//   firstName: "John",
+//   lastName: "Doe",
+//   age: 50,
+//   fullName: function() {
+//     return this.firstName + " " + this.lastName+ " "+ this.age;
+//   }
+// };
+// console.log(person.fullName());
+
+const person = {
+  firstname: "John",
+  lastname: "Doe",
+  age: 50,
+};
+fullname: function(){
+    
+}
+delete person.age;
+
+console.log(person.firstname);
