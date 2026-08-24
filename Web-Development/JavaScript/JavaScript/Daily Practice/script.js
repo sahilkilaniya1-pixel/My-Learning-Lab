e2a370405336a26eb23a69834b0bfb2ae42802aa
Sahil <sahilkilaniya1@gmail.com>
2026-08-24 14:46:04 +0530

    commit

---
+++ Web-Development/JavaScript/JavaScript/Daily Practice/script.js
@@ -0,0 +1,9 @@
+const inputFiled = document.querySelector('#userInput');
+const copyButton = document.querySelector('#copyBtn');
+const statusMessage = document.querySelector('#statusMsg');
+
+function copyToClipboard(text){
+    navigation.clipboard.writeText(text) {
+        
+    }
+}
