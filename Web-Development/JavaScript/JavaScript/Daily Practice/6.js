4ab6503ab3314d37d5739501317696916a5e4316
Sahil <sahilkilaniya1@gmail.com>
2026-08-25 11:15:02 +0530

    commit

--- Web-Development/JavaScript/JavaScript/Daily Practice/6.js
+++ Web-Development/JavaScript/JavaScript/Daily Practice/6.js
@@ -2,4 +2,5 @@ function printTriangle(N) {
   for (let i = 1; i <= N; i++) {       // Rule 1: Row tracker
     let row = "";                      // Rule 2: Khali Dibba
     
-    
+    for (let j = 1; j <= i; j++) {     // Rule 3: Column tracker
+     
