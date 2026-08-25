9d4aa5c82375dc44e0a5371235360794b857ed58
Sahil <sahilkilaniya1@gmail.com>
2026-08-25 11:18:19 +0530

    commit

--- Web-Development/JavaScript/JavaScript/Daily Practice/6.js
+++ Web-Development/JavaScript/JavaScript/Daily Practice/6.js
@@ -13,13 +13,4 @@
 
 
 function printTriangle(n) {
-    for(let i=1; i<=n; i++){
-        let row ="";
-
-        for(let j=1; j<=i; j++){
-            row+="*";
-        }
-        console.log(row);
-    }
-}
-printTriangle(4);
+    
