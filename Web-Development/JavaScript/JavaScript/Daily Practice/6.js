c4b6f669280f2b75c905923b10188e7ae96a9c22
Sahil <sahilkilaniya1@gmail.com>
2026-08-26 16:46:13 +0530

    commit

--- Web-Development/JavaScript/JavaScript/Daily Practice/6.js
+++ Web-Development/JavaScript/JavaScript/Daily Practice/6.js
@@ -74,17 +74,28 @@
 // }
 // printSolidSquare(5);
 
-function printPaddedTriangle(n) {
+// function printPaddedTriangle(n) {
+//     for (let i = 1; i <= n; i++) {
+//         let row = "";
+//         for (let s = 1; s <= (n - i); s++) {
+//             row += " ";
+//         }
+//         for (let j = 1; j <= i; j++) {
+//             row += "*";
+//         }
+
+//         console.log(row);
+//     }
+// }
+// printPaddedTriangle(4);
+
+// function SolidSquare(n) {
     for (let i = 1; i <= n; i++) {
         let row = "";
-        for (let s = 1; s <= (n - i); s++) {
-            row += " ";
-        }
-        for (let j = 1; j <= i; j++) {
+        for (let j = 1; j <= n; j++) {
             row += "*";
         }
-
         console.log(row);
     }
 }
-printPaddedTriangle(4);
+SolidSquare(5);
