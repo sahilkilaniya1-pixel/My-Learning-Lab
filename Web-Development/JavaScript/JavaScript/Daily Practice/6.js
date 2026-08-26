173968b6de426eb815bdfffd4b327cabc9dd184b
Sahil <sahilkilaniya1@gmail.com>
2026-08-26 10:00:25 +0530

    commit

--- Web-Development/JavaScript/JavaScript/Daily Practice/6.js
+++ Web-Development/JavaScript/JavaScript/Daily Practice/6.js
@@ -61,3 +61,14 @@
 //     }
 // }
 // printPaddedTriangle(4);
+
+function printSolidSquare(n) {
+//     for (let i = 1; i <= n; i++) {
+//         let row = "";
+//         for (let j = 1; j <= n; j++) {
+//             row += "*";
+//         }
+//         console.log(row); 
+//     }
+// }
+// printSolidSquare(5);
