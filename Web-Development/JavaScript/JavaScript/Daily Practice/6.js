65b1ffc5f6895bdd2771cbc25825f77804b5c2b1
Sahil <sahilkilaniya1@gmail.com>
2026-08-26 17:24:15 +0530

    commit

--- Web-Development/JavaScript/JavaScript/Daily Practice/6.js
+++ Web-Development/JavaScript/JavaScript/Daily Practice/6.js
@@ -114,3 +114,18 @@
     
 // }
 // printPattern(5);
+
+
+// function  LeftParallelogram(n){
+    for(let i=1; i<=n; i++){
+        let row = "";
+        for(let s=1; s<=(n-i+1); s++){
+            row+=" ";
+        }
+        for(let j=1;j<=n; j++){
+            row+="*";
+        }
+        console.log(row);
+    }
+}
+ LeftParallelogram(5);
