f156873b8ec97d02756238f204cfea9468b737fb
Sahil <sahilkilaniya1@gmail.com>
2026-08-26 17:38:19 +0530

    commit

--- Web-Development/JavaScript/JavaScript/Daily Practice/6.js
+++ Web-Development/JavaScript/JavaScript/Daily Practice/6.js
@@ -117,15 +117,30 @@
 
 
 // function  LeftParallelogram(n){
-    // for(let i=1; i<=n; i++){
-        // let row = "";
-        // for(let s=1; s<=(n-i+1); s++){
-            // row+=" ";
-        // }
-        // for(let j=1;j<=n; j++){
-            // row+="*";
-        // }
-        // console.log(row);
-    // }
+//     for(let i=1; i<=n; i++){
+//         let row = "";
+//         for(let s=1; s<=(n-i+1); s++){
+//             row+=" ";
+//         }
+//         for(let j=1;j<=n; j++){
+//             row+="*";
+//         }
+//         console.log(row);
+//     }
 // }
 //  LeftParallelogram(5);
+
+
+// function  RightParallelogram(n){
+    for(let i=1; i<=n; i++){
+        let row = "";
+        for(let s=1; s<=(i-1); s++){
+            row+=" ";
+        }
+        for(let j=1;j<=n; j++){
+            row+="*";
+        }
+        console.log(row);
+    }
+}
+RightParallelogram(5);
