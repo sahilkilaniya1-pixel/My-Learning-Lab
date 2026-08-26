fe99feee27c90d811bd1808ec71eb86f27a7c6bf
Sahil <sahilkilaniya1@gmail.com>
2026-08-26 18:48:23 +0530

    commit

--- Web-Development/JavaScript/JavaScript/Daily Practice/6.js
+++ Web-Development/JavaScript/JavaScript/Daily Practice/6.js
@@ -149,13 +149,13 @@
 
 // function WideSpaceTriangle(n) {
 //   for (let i = 1; i <= n; i++) {
-    // let row = "";
-    // for (let s = 1; s <= (n - i); s++) {
-    //   row += "  ";
-    // }
-    // for (let j = 1; j <= i; j++) {
-    //   row += "* ";
-    // }
+// let row = "";
+// for (let s = 1; s <= (n - i); s++) {
+//   row += "  ";
+// }
+// for (let j = 1; j <= i; j++) {
+//   row += "* ";
+// }
 //     console.log(row);
 //   }
 // }
--- Web-Development/JavaScript/JavaScript/Practice-JS/04-logic-building-and-patterns/day-21-javascript-pattern.js
+++ Web-Development/JavaScript/JavaScript/Practice-JS/04-logic-building-and-patterns/day-21-javascript-pattern.js
@@ -88,17 +88,19 @@
 
 
 // Q12. Right Parallelogram
-// let n =5;
-// for(let i=1; i<=n; i++){
-//     let row="";
-//     for(let j=1; j<=i; j++){
-//         row+=" ";
+// function RightParallelogram(n) {
+//     for (let i = 1; i <= n; i++) {
+//         let row = "";
+//         for (let s = 1; s <= (i - 1); s++) {
+//             row += " ";
+//         }
+//         for (let j = 1; j <= n; j++) {
+//             row += "*";
+//         }
+//         console.log(row);
 //     }
-//     for(let s=1; s<=n; s++){
-//         row+="*";
-//     }
-//     console.log(row);
 // }
+// RightParallelogram(5);
 
 
 
