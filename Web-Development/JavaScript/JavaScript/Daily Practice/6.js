dc93915fd7bf6099661f8370b76401d867395e29
Sahil <sahilkilaniya1@gmail.com>
2026-08-26 09:49:31 +0530

    commit

--- Web-Development/JavaScript/JavaScript/Daily Practice/6.js
+++ Web-Development/JavaScript/JavaScript/Daily Practice/6.js
@@ -27,7 +27,7 @@ printTriangle(4);
 
 function printArray(a) 
 {
-//      var len = a.length, i = 0;
+     var len = a.length, i = 0;
 //      if (len == 0)
 //         console.log("Empty Array");
 //      else 
