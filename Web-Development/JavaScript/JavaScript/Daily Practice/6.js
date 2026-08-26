c487790ae7f6b3bac9378116ae0533fd924b5aa3
Sahil <sahilkilaniya1@gmail.com>
2026-08-26 16:46:37 +0530

    commit

--- Web-Development/JavaScript/JavaScript/Daily Practice/6.js
+++ Web-Development/JavaScript/JavaScript/Daily Practice/6.js
@@ -93,7 +93,7 @@
     // for (let i = 1; i <= n; i++) {
         // let row = "";
         // for (let j = 1; j <= n; j++) {
-            row += "*";
+            // row += "*";
         }
         console.log(row);
     }
