e398471e048811a05fdbce765495bb9572757b35
Sahil <sahilkilaniya1@gmail.com>
2026-08-26 19:40:24 +0530

    commit

--- Web-Development/JavaScript/JavaScript/Daily Practice/6.js
+++ Web-Development/JavaScript/JavaScript/Daily Practice/6.js
@@ -183,3 +183,28 @@
 //     }
 // }
 // PrintInvertedRightTriangle(6);
+
+
+// function  DoubleStepTriangle(n){
+//     for(let i=1; i<=n; i++){
+//         let row ="";
+//         for(let j=1; j<=2*i-1; j++){
+//             row+="*";
+//         }
+//         console.log(row);
+//     }
+// }
+// DoubleStepTriangle(5);
+
+
+// function AlternateLineTriangle(n) {
+//     for (let i = 1; i <= n; i++) {
+//         let row = "";
+//         for (let j = 1; j <= (i % 2 !== 0 ? 1 : 2); j++) {
+//             row += "*";
+//         }
+
+//         console.log(row);
+//     }
+// }
+// AlternateLineTriangle(5);
--- Web-Development/JavaScript/JavaScript/Practice-JS/04-logic-building-and-patterns/day-21-javascript-pattern.js
+++ Web-Development/JavaScript/JavaScript/Practice-JS/04-logic-building-and-patterns/day-21-javascript-pattern.js
@@ -1,40 +1,65 @@
 // Q1. Solid Square
-// function SolidSquare(n) {
-//     for (let i = 1; i <= n; i++) {
-//         let row = "";
-//         for (let j = 1; j <= n; j++) {
-//             row += "*";
-//         }
-//         console.log(row);
-//     }
-// }
-// SolidSquare(5);
+function SolidSquare(n) {
+    for (let i = 1; i <= n; i++) {
+        let row = "";
+        for (let j = 1; j <= n; j++) {
+            row += "*";
+        }
+        console.log(row);
+    }
+}
+SolidSquare(5);
 
 // Q2. RightAngleTriangle
-// function PrintRightAngleTriangle(n) {
-//     for (let i = 1; i <= n; i++) {
-//         let row = "";
-//         for (let j = 1; j <= i; j++) {
-//             row += "*";
-//         }
-//         console.log(row);
-//     }
-// }
-// PrintRightAngleTriangle(5);
+function PrintRightAngleTriangle(n) {
+    for (let i = 1; i <= n; i++) {
+        let row = "";
+        for (let j = 1; j <= i; j++) {
+            row += "*";
+        }
+        console.log(row);
+    }
+}
+PrintRightAngleTriangle(5);
 
 
 // Q3. InvertedRightTriangle
-// function PrintInvertedRightTriangle(n) {
-//     for (let i = 1; i <= n; i++) {
-//         let row = "";
-//         for (let j = 1; j <= n - i; j++) {
-//             row += "*";
-//         }
-//         console.log(row);
-//     }
-// }
-// PrintInvertedRightTriangle(6);
+function PrintInvertedRightTriangle(n) {
+    for (let i = 1; i <= n; i++) {
+        let row = "";
+        for (let j = 1; j <= n - i; j++) {
+            row += "*";
+        }
+        console.log(row);
+    }
+}
+PrintInvertedRightTriangle(6);
 
+
+// Q4. DoubleStepTriangle
+function DoubleStepTriangle(n) {
+    for (let i = 1; i <= n; i++) {
+        let row = "";
+        for (let j = 1; j <= 2 * i - 1; j++) {
+            row += "*";
+        }
+        console.log(row);
+    }
+}
+DoubleStepTriangle(5);
+
+// Q5. Alternate Line Triangle
+function AlternateLineTriangle(n) {
+    for (let i = 1; i <= n; i++) {
+        let row = "";
+        for (let j = 1; j <= (i % 2 !== 0 ? 1 : 2); j++) {
+            row += "*";
+        }
+
+        console.log(row);
+    }
+}
+AlternateLineTriangle(5);
 
 
 // Q7 .Horizontal Triangle
