bdbffac055148b4c6a4cf0bb86cef2c53f6cc33a
Sahil <sahilkilaniya1@gmail.com>
2026-08-26 18:17:21 +0530

    commit

--- Web-Development/JavaScript/JavaScript/Daily Practice/6.js
+++ Web-Development/JavaScript/JavaScript/Daily Practice/6.js
@@ -150,7 +150,7 @@
 // function WideSpaceTriangle(n) {
 //   for (let i = 1; i <= n; i++) {
     // let row = "";
-    for (let s = 1; s <= (n - i); s++) {
+    // for (let s = 1; s <= (n - i); s++) {
       row += "  ";
     }
     for (let j = 1; j <= i; j++) {
