344debaf19598888509febb46dcfa61d763a90bf
Sahil <sahilkilaniya1@gmail.com>
2026-08-26 16:46:26 +0530

    commit

--- Web-Development/JavaScript/JavaScript/Daily Practice/6.js
+++ Web-Development/JavaScript/JavaScript/Daily Practice/6.js
@@ -91,7 +91,7 @@
 
 // function SolidSquare(n) {
     // for (let i = 1; i <= n; i++) {
-        let row = "";
+        // let row = "";
         for (let j = 1; j <= n; j++) {
             row += "*";
         }
