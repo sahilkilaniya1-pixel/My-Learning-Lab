bf96101461235e8f45682e6e986c15747b898466
Sahil <sahilkilaniya1@gmail.com>
2026-08-26 16:46:19 +0530

    commit

--- Web-Development/JavaScript/JavaScript/Daily Practice/6.js
+++ Web-Development/JavaScript/JavaScript/Daily Practice/6.js
@@ -90,7 +90,7 @@
 // printPaddedTriangle(4);
 
 // function SolidSquare(n) {
-    for (let i = 1; i <= n; i++) {
+    // for (let i = 1; i <= n; i++) {
         let row = "";
         for (let j = 1; j <= n; j++) {
             row += "*";
