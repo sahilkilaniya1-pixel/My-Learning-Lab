e642dde486268fe84b38387de9cf64f6fadb7430
Sahil <sahilkilaniya1@gmail.com>
2026-08-26 09:51:10 +0530

    commit

--- Web-Development/JavaScript/JavaScript/Daily Practice/6.js
+++ Web-Development/JavaScript/JavaScript/Daily Practice/6.js
@@ -42,7 +42,7 @@ do-while loop in javascript
 
 for (let i = 1; i <= 3; i++) {
   let row = "";
-//   for (let j = 1; j <= 4; j++) {
+  for (let j = 1; j <= 4; j++) {
 //     row += j + " ";
 //   }
 //   console.log(row);
