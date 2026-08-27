d1c4712763c286cf18b8421e5a5ad08779b1422b
Sahil <sahilkilaniya1@gmail.com>
2026-08-27 09:35:34 +0530

    commit

--- Web-Development/JavaScript/JavaScript/Practice-JS/04-logic-building-and-patterns/day-21-javascript-pattern.js
+++ Web-Development/JavaScript/JavaScript/Practice-JS/04-logic-building-and-patterns/day-21-javascript-pattern.js
@@ -171,7 +171,7 @@ AlternateLineTriangle(5);
 // function MirroredHalfDiamond(n) {
     // for (let i = 1; i <= n; i++) {
         // let row = "";
-        // for (let s = 1; s <= (n - i); s++) {
+        for (let s = 1; s <= (n - i); s++) {
             row += " ";
         }
         for (let j = 1; j <= i; j++) {
