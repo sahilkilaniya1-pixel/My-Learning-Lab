516453a36a71fa6b6e67fcdfdbcdff02d0fbbcd7
Sahil <sahilkilaniya1@gmail.com>
2026-08-27 09:33:20 +0530

    commit

--- Web-Development/JavaScript/JavaScript/Practice-JS/04-logic-building-and-patterns/day-21-javascript-pattern.js
+++ Web-Development/JavaScript/JavaScript/Practice-JS/04-logic-building-and-patterns/day-21-javascript-pattern.js
@@ -181,7 +181,7 @@ AlternateLineTriangle(5);
 // }
 // for (let i = n - 1; i >= 1; i--) {
 // let row = "";
-// for (let s = 1; s <= (n - i); s++) {
+for (let s = 1; s <= (n - i); s++) {
 row += " ";
 }
 for (let j = 1; j <= i; j++) {
