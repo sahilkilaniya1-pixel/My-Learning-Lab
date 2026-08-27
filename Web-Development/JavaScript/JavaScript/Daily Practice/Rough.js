1e30c8db6f1e80c43c843ca2e15ab44c6b7c061d
Sahil <sahilkilaniya1@gmail.com>
2026-08-27 09:27:36 +0530

    commit

--- Web-Development/JavaScript/JavaScript/Daily Practice/Rough.js
+++ Web-Development/JavaScript/JavaScript/Daily Practice/Rough.js
@@ -226,7 +226,7 @@
         // let row = "";
         // for (let s = 1; s <= (n - i); s++) {
             // row += " ";
-        }
+        // }
         for (let j = 1; j <= i; j++) {
             row += "*";
         }
