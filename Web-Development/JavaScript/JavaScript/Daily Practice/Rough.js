087ac3071b46ba198cf09201205da9d2e70f9ceb
Sahil <sahilkilaniya1@gmail.com>
2026-08-27 15:41:46 +0530

    commit

--- Web-Development/JavaScript/JavaScript/Daily Practice/Rough.js
+++ Web-Development/JavaScript/JavaScript/Daily Practice/Rough.js
@@ -309,16 +309,16 @@
 // }
 // ShiftedSquare(3);
 
-function InvertedPyramid(n) {
-    for (let i = 1; i <= n; i++) {
+function InvertedPyramidSolid(n) {
+    for (let i = n; i >= 1; i--) {
         let row = "";
-        for (let s = 1; s <= (n*i); s++) {
+        for (let s = 1; s <= (n - i); s++) {
             row += " ";
         }
-        for (let j = 1; j <= i; j++) {
-            row += "* ";
+        for (let j = 1; j <= (2 * i - 1); j++) {
+            row += "*";
         }
         console.log(row);
     }
 }
-InvertedPyramid(5);
+InvertedPyramidSolid(5);
--- Web-Development/JavaScript/JavaScript/Practice-JS/04-logic-building-and-patterns/Day-40-Pyramids & Diamonds Q16 - Q23.js
+++ Web-Development/JavaScript/JavaScript/Practice-JS/04-logic-building-and-patterns/Day-40-Pyramids & Diamonds Q16 - Q23.js
@@ -14,3 +14,18 @@ function FullPyramid(n) {
 FullPyramid(5);
 
 // Q17. Inverted Pyramid
+function InvertedPyramidSolid(n) {
+    for (let i = n; i >= 1; i--) {
+        let row = "";
+        for (let s = 1; s <= (n - i); s++) {
+            row += " ";
+        }
+        for (let j = 1; j <= (2 * i - 1); j++) {
+            row += "*";
+        }
+        console.log(row);
+    }
+}
+InvertedPyramidSolid(5);
+
+
