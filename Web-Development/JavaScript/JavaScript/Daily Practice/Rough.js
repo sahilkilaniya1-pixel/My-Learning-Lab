70b692295795e83c995370285d9b3e3261e853fe
Sahil <sahilkilaniya1@gmail.com>
2026-08-29 11:09:12 +0530

    commit

--- Web-Development/JavaScript/JavaScript/Daily Practice/Rough.js
+++ Web-Development/JavaScript/JavaScript/Daily Practice/Rough.js
@@ -346,7 +346,7 @@
     // let sum = 0;
     // for(let key in obj){
         // if(typeof obj[key]==='number'){
-            sum +=obj[key];
+            // sum +=obj[key];
         }
     }
     return sum;
