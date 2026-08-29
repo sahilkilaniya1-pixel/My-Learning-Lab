59e42ae6f70de3a300c96383fcaea68d880bf24e
Sahil <sahilkilaniya1@gmail.com>
2026-08-29 15:11:39 +0530

    commit

--- Web-Development/JavaScript/JavaScript/Daily Practice/Rough.js
+++ Web-Development/JavaScript/JavaScript/Daily Practice/Rough.js
@@ -406,3 +406,12 @@
 // }
 // console.log(greetUser("Sahil"));
 // console.log(greetUser());
+
+// function sumAll (...numbers){
+//     let total = 0;
+//     for(let num of numbers){
+//         total +=num;
+//     }
+//     return total;
+// }
+// console.log(sumAll(5,10));
