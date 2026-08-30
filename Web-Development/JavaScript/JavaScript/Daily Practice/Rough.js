7464286470e27b4a3111ae3af28458e841a79309
Sahil <sahilkilaniya1@gmail.com>
2026-08-30 17:42:35 +0530

    commit

--- Web-Development/JavaScript/JavaScript/Daily Practice/Rough.js
+++ Web-Development/JavaScript/JavaScript/Daily Practice/Rough.js
@@ -447,4 +447,4 @@ const { use } = require("react");
 // for...of vs for...in
 // const arr = [10, 20];
 // for (let val of arr) console.log(val); // 10, 20 (Values)
-for (let index in arr) console.log(index); // 0, 1 (Keys/Indices)
+// for (let index in arr) console.log(index); // 0, 1 (Keys/Indices)
