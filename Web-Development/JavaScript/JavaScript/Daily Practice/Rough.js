62ec9c47ff7988669334096dc72f6e7908d6c09d
Sahil <sahilkilaniya1@gmail.com>
2026-08-30 18:13:28 +0530

    commit

--- Web-Development/JavaScript/JavaScript/Daily Practice/Rough.js
+++ Web-Development/JavaScript/JavaScript/Daily Practice/Rough.js
@@ -471,7 +471,7 @@ const { use } = require("react");
 // let numbers = [1, 2, 3, 4];
 // let squared = numbers.map(num => num * num);
 // console.log(squared);
-let evenNumber = numbers.filter(num => num % 2 === 0);
+// let evenNumber = numbers.filter(num => num % 2 === 0);
 console.log(evenNumber);
 let sum = numbers.reduce((accumulator, current) => accumulator + current, 0);
 console.log(sum);
