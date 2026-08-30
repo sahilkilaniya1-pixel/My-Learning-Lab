2500eb64e0f45b45493990a56d252e9035b6d608
Sahil <sahilkilaniya1@gmail.com>
2026-08-30 22:43:44 +0530

    commit

--- Web-Development/JavaScript/JavaScript/Daily Practice/Rough.js
+++ Web-Development/JavaScript/JavaScript/Daily Practice/Rough.js
@@ -535,7 +535,7 @@ const { use } = require("react");
 // console.log(Object.entries(user));
 
 // const obj1 = {a:1};
-const obj2 = {b:2};
+// const obj2 = {b:2};
 
 const merged = {...obj1, ...obj2};
 console.log(merged);
