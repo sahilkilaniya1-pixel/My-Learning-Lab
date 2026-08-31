63c5300c9f9b6b47c10d2104a0d53945871d6ff7
Sahil <sahilkilaniya1@gmail.com>
2026-09-01 00:03:30 +0530

    commit

--- Web-Development/JavaScript/JavaScript/Daily Practice/Function Rough.js
+++ Web-Development/JavaScript/JavaScript/Daily Practice/Function Rough.js
@@ -39,7 +39,7 @@
 // function sumOld() {
     // let total = 0;
     // for (let i = 0; i < arguments.length; i++) {
-        total += arguments[i];
+        // total += arguments[i];
     }
     return total;
 }
