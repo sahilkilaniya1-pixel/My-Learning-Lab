7bcea0d7dccb69914bf180afba3340b3b52f3952
Sahil <sahilkilaniya1@gmail.com>
2026-08-31 21:52:25 +0530

    commit

--- Web-Development/JavaScript/JavaScript/Daily Practice/Object Rough.js
+++ Web-Development/JavaScript/JavaScript/Daily Practice/Object Rough.js
@@ -18,7 +18,7 @@
 
 // B. Object.assign() aur Spread Operator (...)
 // const obj1 = {a:1};
-const obj2 = {b:2};
+// const obj2 = {b:2};
 // Spread Operator (Modern Way)
 const merged = {...obj1, ...obj2};
 console.log(merged);
