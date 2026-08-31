54b70a4755cca08a46887d6ee32fbe86cc474b3d
Sahil <sahilkilaniya1@gmail.com>
2026-08-31 23:21:08 +0530

    commit

--- Web-Development/JavaScript/JavaScript/Daily Practice/Object Rough.js
+++ Web-Development/JavaScript/JavaScript/Daily Practice/Object Rough.js
@@ -51,4 +51,9 @@
 const defaultSettings = {theme: "light", font:"arial"};
 const userSettings = {theme: "dark"};
 
-function customAssign(target, source ){}
+function customAssign(target, source ){
+    for(let key in source) {
+        target[key] = source[key];
+    }
+    return 
+}
