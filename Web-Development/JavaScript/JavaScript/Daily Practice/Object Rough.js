fcb5248ee83f2669a0a79dd5a56523ad6b2272ac
Sahil <sahilkilaniya1@gmail.com>
2026-08-31 21:37:45 +0530

    commit

--- Web-Development/JavaScript/JavaScript/Daily Practice/Object Rough.js
+++ Web-Development/JavaScript/JavaScript/Daily Practice/Object Rough.js
@@ -1,2 +1,11 @@
 // 1. Object Creation & Accessing
 // A. Creation (Object Literal)
+const person = {
+    name: "Sahil",
+    age: 22,
+    isCoder: true
+};
+console.log(person);
+console.log(person.name);
+console.log(person["age"]);
+// Dynamic key Access Example
