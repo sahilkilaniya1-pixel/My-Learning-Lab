0837585772f71ba912922418c448dc440a0fdf4c
Sahil <sahilkilaniya1@gmail.com>
2026-08-31 21:43:21 +0530

    commit

--- Web-Development/JavaScript/JavaScript/Daily Practice/Object Rough.js
+++ Web-Development/JavaScript/JavaScript/Daily Practice/Object Rough.js
@@ -11,4 +11,7 @@
 // Dynamic key Access Example
 
 // 2. Objects WITH Built-in Methods
-const user = {name: "Rahul", city:"Rohtak", role:"Developer"};
+// const user = {name: "Rahul", city:"Rohtak", role:"Developer"};
+console.log(Object.keys(user));
+console.log(Object.values(user));
+console.log(Object.entries(user));
