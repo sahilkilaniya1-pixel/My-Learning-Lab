802be6b42ef8dbb20dd451b5894f83872679be4a
Sahil <sahilkilaniya1@gmail.com>
2026-09-01 00:09:22 +0530

    commit

--- Web-Development/JavaScript/JavaScript/Daily Practice/Function Rough.js
+++ Web-Development/JavaScript/JavaScript/Daily Practice/Function Rough.js
@@ -49,7 +49,7 @@
 // B. Modern Way: Rest Parameters (...args)
 // function sumNew(...numbers){
     // return numbers.reduce((total,num) => total +num, 0);
-}
+// }
 // console.log(sumNew(10,20,30,40)); // 100
 
 //Specific parameters ke sath:
