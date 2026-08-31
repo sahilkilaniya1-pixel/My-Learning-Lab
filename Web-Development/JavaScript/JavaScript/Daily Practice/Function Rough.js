af52bdfc61a29fff853ac7a2bee1c04291b9ba1a
Sahil <sahilkilaniya1@gmail.com>
2026-08-31 23:27:14 +0530

    commit

--- Web-Development/JavaScript/JavaScript/Daily Practice/Function Rough.js
+++ Web-Development/JavaScript/JavaScript/Daily Practice/Function Rough.js
@@ -1,2 +1,3 @@
 // JavaScript Functions Deep Dive
 // Function ek reusable code block hota hai jo specific task perform karta hai. Code repeating se bachne aur logic ko modular banane ke liye hum functions ka use karte hain.
+
