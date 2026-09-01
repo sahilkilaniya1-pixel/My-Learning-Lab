b2e0a9fa8f5c82332390614c1475bcbc085beb8e
Sahil <sahilkilaniya1@gmail.com>
2026-09-02 01:21:36 +0530

    commit

--- index.php
+++ index.php
@@ -56,8 +56,8 @@ $all_users = $stmt->fetchAll(PDO::FETCH_ASSOC);
     <meta charset="UTF-8">
     <title>PHP MySQL Easy Demo</title>
     <style>
-        /* body { font-family: Arial, sans-serif; margin: 30px; background: #f4f4f4; } */
-        .box { background: white; padding: 20px; border-radius: 8px; margin-bottom: 20px; max-width: 500px; }
+        body { font-family: Arial, sans-serif; margin: 30px; background: #f4f4f4; }
+        /* .box { background: white; padding: 20px; border-radius: 8px; margin-bottom: 20px; max-width: 500px; } */
         input { width: 90%; padding: 8px; margin: 8px 0; }
         button { background: green; color: white; padding: 10px 15px; border: none; cursor: pointer; }
         table { width: 100%; border-collapse: collapse; margin-top: 10px; }
