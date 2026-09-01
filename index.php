fe44ec028d93fb28a7edf0b9930e6be779d34d9d
Sahil <sahilkilaniya1@gmail.com>
2026-09-02 01:15:25 +0530

    commit

--- index.php
+++ index.php
@@ -11,8 +11,8 @@ try {
     // DB Line Connect karna
     $pdo = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8mb4", $username, $password);
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-// } catch (PDOException $e) {
-    die("Database Connection Failed: " . $e->getMessage());
+} catch (PDOException $e) {
+    // die("Database Connection Failed: " . $e->getMessage());
 }
 
 // ==========================================
