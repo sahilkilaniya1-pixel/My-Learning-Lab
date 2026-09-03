08e72c13eb83ede7fb2473ea23bf639c57d7bf11
Sahil <sahilkilaniya1@gmail.com>
2026-09-03 21:25:56 +0530

    commit

--- index.php
+++ index.php
@@ -29,8 +29,8 @@
     if (isset($_POST['logout_session_btn'])) {
         session_unset();
         session_destroy();
-        // header("Location: " . $_SERVER['PHP_SELF']);
-        exit;
+        header("Location: " . $_SERVER['PHP_SELF']);
+        // exit;
     }
 ?>
 <!DOCTYPE html>
