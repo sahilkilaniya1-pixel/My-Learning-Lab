5ad6e95e785f615b4f1ab3f5e12c5749ee7c6939
Sahil <sahilkilaniya1@gmail.com>
2026-09-03 21:22:08 +0530

    commit

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
-<?php
-    // Sessions use karne ke liye session_start() sabse pehle (HTML se pehle) hona chahiye
+<!-- <?php -->
+    Sessions use karne ke liye session_start() sabse pehle (HTML se pehle) hona chahiye
     if (session_status() === PHP_SESSION_NONE) {
         session_start();
     }
