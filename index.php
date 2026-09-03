1e3d8e63d092f7880d7188255d670d519bb649da
Sahil <sahilkilaniya1@gmail.com>
2026-09-03 21:23:37 +0530

    commit

--- index.php
+++ index.php
@@ -10,9 +10,9 @@
         setcookie("user_preferred_theme", $theme, time() + (86400 * 7), "/"); // 7 days valid
         header("Location: " . $_SERVER['PHP_SELF']);
         exit;
-    // }
+    }
 
-    if (isset($_POST['delete_cookie_btn'])) {
+    // if (isset($_POST['delete_cookie_btn'])) {
         setcookie("user_preferred_theme", "", time() - 3600, "/"); // Past time sets deletion
         header("Location: " . $_SERVER['PHP_SELF']);
         exit;
