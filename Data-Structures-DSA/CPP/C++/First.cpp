4de29d34defae332017b40a74dc55b582f5b9dc4
Sahil <sahilkilaniya1@gmail.com>
2026-09-06 21:20:13 +0530

    commit

--- Data-Structures-DSA/CPP/C++/First.cpp
+++ Data-Structures-DSA/CPP/C++/First.cpp
@@ -3,6 +3,18 @@
 
 // int main()
 // {
-    // cout << "Hello coders";
-    // return 0;
+// cout << "Hello coders";
+// return 0;
 // }
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    string name;
+    cout << "enter your good name";
+    cin >> name;
+    cout << "Hello" << name;
+    return 0;
+}
