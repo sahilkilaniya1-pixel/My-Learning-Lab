72315584e6191bb5b85a1043c0ed07a78e011f24
Sahil <sahilkilaniya1@gmail.com>
2026-09-06 21:22:05 +0530

    commit

--- Data-Structures-DSA/CPP/C++/First.cpp
+++ Data-Structures-DSA/CPP/C++/First.cpp
@@ -8,13 +8,15 @@
 // }
 
 #include <iostream>
+#include <string>
 using namespace std;
 
 int main()
 {
     string name;
-    cout << "enter your good name";
-    cin >> name;
-    cout << "Hello" << name;
+    cout << "Enter your good name: ";
+    getline(cin, name);
+    
+    cout << "Hello " << name;
     return 0;
 }
