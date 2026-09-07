83b471cf1466a778b685d14209b37325ad4bde11
Sahil <sahilkilaniya1@gmail.com>
2026-09-07 08:35:14 +0530

    commit

--- Data-Structures-DSA/CPP/C++/Variables.cpp
+++ Data-Structures-DSA/CPP/C++/Variables.cpp
@@ -1,3 +1,4 @@
 // A local variable is declared inside a function or block and can be accessed only within that block.
 // A global variable is declared outside all functions and can be accessed from  any function in the program
 #include <iostream>
+using namespace std;
