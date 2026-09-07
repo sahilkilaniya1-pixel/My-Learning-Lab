fa5db0b4bc8754def650969e54ecdb1ae89d6e81
Sahil <sahilkilaniya1@gmail.com>
2026-09-07 08:11:12 +0530

    commit

--- Data-Structures-DSA/CPP/C++/First.cpp
+++ Data-Structures-DSA/CPP/C++/First.cpp
@@ -7,16 +7,25 @@
 // return 0;
 // }
 
-#include <iostream>
+// #include <iostream>
 #include <string>
 using namespace std;
 
 int main()
 {
     string name;
-    cout << "Enter your good name: ";
+    int age;
+    string gender;
+
+    cout << "Enter your good name:";
     getline(cin, name);
-    
-    cout << "Hello " << name;
+
+    cout << "Enter Your Age:";
+    cin >> age;
+
+    cout << "Enter Your Gender:";
+    cin >> gender;
+
+    cout << "\nHello " << name << " Age " << age << " Gender " << gender << endl;
     return 0;
 }
