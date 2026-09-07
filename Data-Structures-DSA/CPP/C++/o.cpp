4b20320952677fd4bcccd9863f60c285efe9fce8
Sahil <sahilkilaniya1@gmail.com>
2026-09-07 08:25:30 +0530

    commit

---
+++ Data-Structures-DSA/CPP/C++/o.cpp
@@ -0,0 +1,32 @@
+// #include <iostream>
+// using namespace std;
+
+// int main()
+// {
+// cout << "Hello coders";
+// return 0;
+// }
+
+// #include <iostream>
+// #include <string>
+// using namespace std;
+
+// int main()
+// {
+// string name;
+// int age;
+// string gender;
+
+// cout << "Enter your good name:";
+// getline(cin, name);
+
+// cout << "Enter Your Age:";
+// cin >> age;
+
+// cout << "Enter Your Gender:";
+// cin >> gender;
+
+// cout << "\nHello " << name << " Age " << age << " Gender " << gender << endl;
+// return 0;
+// }
+
