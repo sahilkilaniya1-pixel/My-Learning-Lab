055f61bfef8ce7faefbcd664562e0b6dc6faac21
Sahil <sahilkilaniya1@gmail.com>
2026-09-08 15:45:30 +0530

    commit

--- Data-Structures-DSA/All lang-Practice/practice 21.js
+++ Data-Structures-DSA/All lang-Practice/practice 21.js
@@ -30,45 +30,45 @@
 // }
 // printButterfly(5);
 
-function printDiamondInSquare(n) {
-  if (n % 2 === 0) {
-    n = n + 1;
-  }
-  let topRows = (n + 1) / 2;
-  let starCount = topRows;
-  let spaceCount = 0;
-  for (let r = 1; r <= topRows; r = r + 1) {
-    let rowText = "";
-    for (let s = 1; s <= starCount; s = s + 1) {
-      rowText = rowText + "*";
-    }
-    for (let sp = 1; sp <= spaceCount; sp = sp + 1) {
-      rowText = rowText + " ";
-    }
-    for (let s = 1; s <= starCount; s = s + 1) {
-      rowText = rowText + "*";
-    }
-    console.log(rowText);
-    starCount = starCount - 1;
-    spaceCount = spaceCount + 2;
-  }
-  starCount = 2;
-  spaceCount = n - 3;
-  let bottomRows = topRows - 1;
-  for (let r = 1; r <= bottomRows; r = r + 1) {
-    let rowText = "";
-    for (let s = 1; s <= starCount; s = s + 1) {
-      rowText = rowText + "*";
-    }
-    for (let sp = 1; sp <= spaceCount; sp = sp + 1) {
-      rowText = rowText + " ";
-    }
-    for (let s = 1; s <= starCount; s = s + 1) {
-      rowText = rowText + "*";
-    }
-    console.log(rowText);
-    starCount = starCount + 1;
-    spaceCount = spaceCount - 2;
-  }
-}
-printDiamondInSquare(7);
+// function printDiamondInSquare(n) {
+//   if (n % 2 === 0) {
+//     n = n + 1;
+//   }
+//   let topRows = (n + 1) / 2;
+//   let starCount = topRows;
+//   let spaceCount = 0;
+//   for (let r = 1; r <= topRows; r = r + 1) {
+//     let rowText = "";
+//     for (let s = 1; s <= starCount; s = s + 1) {
+//       rowText = rowText + "*";
+//     }
+//     for (let sp = 1; sp <= spaceCount; sp = sp + 1) {
+//       rowText = rowText + " ";
+//     }
+//     for (let s = 1; s <= starCount; s = s + 1) {
+//       rowText = rowText + "*";
+//     }
+//     console.log(rowText);
+//     starCount = starCount - 1;
+//     spaceCount = spaceCount + 2;
+//   }
+//   starCount = 2;
+//   spaceCount = n - 3;
+//   let bottomRows = topRows - 1;
+//   for (let r = 1; r <= bottomRows; r = r + 1) {
+//     let rowText = "";
+//     for (let s = 1; s <= starCount; s = s + 1) {
+//       rowText = rowText + "*";
+//     }
+//     for (let sp = 1; sp <= spaceCount; sp = sp + 1) {
+//       rowText = rowText + " ";
+//     }
+//     for (let s = 1; s <= starCount; s = s + 1) {
+//       rowText = rowText + "*";
+//     }
+//     console.log(rowText);
+//     starCount = starCount + 1;
+//     spaceCount = spaceCount - 2;
+//   }
+// }
+// printDiamondInSquare(7);
