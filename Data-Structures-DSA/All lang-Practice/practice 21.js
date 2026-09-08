b69f1bca0dad843c9bfa0b98fd1e28b7c5ed7e42
Sahil <sahilkilaniya1@gmail.com>
2026-09-08 15:50:25 +0530

    commit

--- Data-Structures-DSA/All lang-Practice/practice 21.js
+++ Data-Structures-DSA/All lang-Practice/practice 21.js
@@ -61,7 +61,7 @@
     //   rowText = rowText + "*";
     // }
     for (let sp = 1; sp <= spaceCount; sp = sp + 1) {
-      rowText = rowText + " ";
+    //   rowText = rowText + " ";
     }
     for (let s = 1; s <= starCount; s = s + 1) {
       rowText = rowText + "*";
