2e24b1126bdea70b3ad0f8014bb8ef9aa733586f
Sahil <sahilkilaniya1@gmail.com>
2026-09-08 16:00:12 +0530

    commit

--- Data-Structures-DSA/All lang-Practice/practice 21.js
+++ Data-Structures-DSA/All lang-Practice/practice 21.js
@@ -75,3 +75,26 @@
 
 
 
+function SandglassStar(n) {
+  for (let i = 0; i < n; i++) {
+    let row = "";
+    for (let s = 0; s < i; s++) {
+      row += "_";
+    }
+    for (let j = 0; j < 2 * (n - i) - 1; j++) {
+      row += "*";
+    }
+    console.log(row);
+  }
+  for (let i = 1; i < n; i++) {
+    let row = "";
+    for (let s = 0; s < n - 1 - i; s++) {
+      row += "_";
+    }
+    for (let j = 0; j < 2 * i + 1; j++) {
+      row += "*";
+    }
+    console.log(row);
+  }
+}
+SandglassStar(5);
