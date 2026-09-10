1af92366980237bf69700bff5e7f57bb4d266002
Sahil <sahilkilaniya1@gmail.com>
2026-09-10 15:49:31 +0530

    commit

---
+++ Web-Development/Practice-JS/04-logic-building-and-patterns/practice.js
@@ -0,0 +1,13 @@
+function NumberPyramid(n) {
+  for (let i = 1; i <= n; i++) {
+    let row = "";
+    for (let s = 1; s <= n - i; s++) {
+      row += " ";
+    }
+    for (let j = 1; j <= i; j++) {
+      row += i + " ";
+    }
+    console.log(row);
+  }
+}
+NumberPyramid(5);
