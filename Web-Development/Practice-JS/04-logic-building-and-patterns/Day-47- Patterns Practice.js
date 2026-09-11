2c302d7b74962b682df0f47ad5316ff4b936dc90
Sahil <sahilkilaniya1@gmail.com>
2026-09-11 08:54:11 +0530

    commit

--- Web-Development/Practice-JS/04-logic-building-and-patterns/Day-47- Patterns Practice.js
+++ Web-Development/Practice-JS/04-logic-building-and-patterns/Day-47- Patterns Practice.js
@@ -16,7 +16,7 @@
 // }
 // printHollowParallelogram(5, 8);
 
-function SameNumberRow(n) {
+// function SameNumberRow(n) {
   for (let i = 1; i <= n; i++) {
     let row = "";
     for (let j = 1; j <= i; j++) {
