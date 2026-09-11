57927968e15bcc8cbdf8d18062af1954e0dc892d
Sahil <sahilkilaniya1@gmail.com>
2026-09-11 08:57:46 +0530

    commit

--- Web-Development/Practice-JS/04-logic-building-and-patterns/Day-47- Patterns Practice.js
+++ Web-Development/Practice-JS/04-logic-building-and-patterns/Day-47- Patterns Practice.js
@@ -54,7 +54,7 @@
 // function InvertedNumbers(n) {
   // for (let i = n; i >= 1; i--) {
     // let row = "";
-    for (let j = 1; j <= i; j++) {
+    // for (let j = 1; j <= i; j++) {
       row += j + " ";
     }
 
