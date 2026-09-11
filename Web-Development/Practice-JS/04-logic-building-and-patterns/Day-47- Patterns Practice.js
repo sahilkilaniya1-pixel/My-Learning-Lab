dd1bdd3de0adf56dd61ef86b92c63b73f3de7d52
Sahil <sahilkilaniya1@gmail.com>
2026-09-11 08:55:25 +0530

    commit

--- Web-Development/Practice-JS/04-logic-building-and-patterns/Day-47- Patterns Practice.js
+++ Web-Development/Practice-JS/04-logic-building-and-patterns/Day-47- Patterns Practice.js
@@ -28,7 +28,7 @@
 // SameNumberRow(4);
 
 // function IncreasingColumn(n) {
-  for (let i = 1; i <= n; i++) {
+  // for (let i = 1; i <= n; i++) {
     let row = "";
     for (let j = 1; j <= i; j++) {
       row += j;
