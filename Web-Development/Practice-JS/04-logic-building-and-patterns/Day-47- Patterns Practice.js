0e7488e7c4edceabcf38ab4898a5198afdc65b6f
Sahil <sahilkilaniya1@gmail.com>
2026-09-11 09:00:01 +0530

    commit

--- Web-Development/Practice-JS/04-logic-building-and-patterns/Day-47- Patterns Practice.js
+++ Web-Development/Practice-JS/04-logic-building-and-patterns/Day-47- Patterns Practice.js
@@ -81,7 +81,7 @@
   // for (let i = 1; i <= n; i++) {
     // let row = "";
     // for (let j = 1; j <= i; j++) {
-      if ((i + j) % 2 === 0) {
+      // if ((i + j) % 2 === 0) {
         row += "1 ";
       } else {
         row += "0 ";
