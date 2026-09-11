f08b6498dd9cc5f79771dc773983565e0353ce2c
Sahil <sahilkilaniya1@gmail.com>
2026-09-11 08:57:35 +0530

    commit

--- Web-Development/Practice-JS/04-logic-building-and-patterns/Day-47- Patterns Practice.js
+++ Web-Development/Practice-JS/04-logic-building-and-patterns/Day-47- Patterns Practice.js
@@ -52,7 +52,7 @@
 // FloydsTriangle(4);
 
 // function InvertedNumbers(n) {
-  for (let i = n; i >= 1; i--) {
+  // for (let i = n; i >= 1; i--) {
     let row = "";
     for (let j = 1; j <= i; j++) {
       row += j + " ";
