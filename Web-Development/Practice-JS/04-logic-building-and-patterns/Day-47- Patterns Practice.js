aa32b0bf99c001895f704f0ddcded04814ad0a8f
Sahil <sahilkilaniya1@gmail.com>
2026-09-11 08:59:46 +0530

    commit

--- Web-Development/Practice-JS/04-logic-building-and-patterns/Day-47- Patterns Practice.js
+++ Web-Development/Practice-JS/04-logic-building-and-patterns/Day-47- Patterns Practice.js
@@ -78,7 +78,7 @@
 // NumberPyramid(5);
 
 // function BinaryTriangle(n) {
-  for (let i = 1; i <= n; i++) {
+  // for (let i = 1; i <= n; i++) {
     let row = "";
     for (let j = 1; j <= i; j++) {
       if ((i + j) % 2 === 0) {
