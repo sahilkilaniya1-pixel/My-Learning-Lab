be81ef7db03159dfea3e9dc68bcc11331f2f9ba4
Sahil <sahilkilaniya1@gmail.com>
2026-09-11 08:58:40 +0530

    commit

--- Web-Development/Practice-JS/04-logic-building-and-patterns/Day-47- Patterns Practice.js
+++ Web-Development/Practice-JS/04-logic-building-and-patterns/Day-47- Patterns Practice.js
@@ -66,7 +66,7 @@
 // function NumberPyramid(n) {
   // for (let i = 1; i <= n; i++) {
     // let row = "";
-    for (let s = 1; s <= n - i; s++) {
+    // for (let s = 1; s <= n - i; s++) {
       row += " ";
     }
     for (let j = 1; j <= i; j++) {
