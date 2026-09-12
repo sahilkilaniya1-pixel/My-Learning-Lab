37913bca169feb61fb2dd6655c797176922e2eaa
Sahil <sahilkilaniya1@gmail.com>
2026-09-12 22:21:30 +0530

    commit

--- Web-Development/Practice-JS/06-Events/index.js
+++ Web-Development/Practice-JS/06-Events/index.js
@@ -44,26 +44,13 @@ const element = document.getElementById("first");
 //   element.style.backgroundColor = "Blue";
 // });
 
-const child1 = document.getElementById("child1");
-child1.addEventListener("click", () => {
-  child1.textContent = "I am clicked";
-});
-child1.addEventListener("click", () => {
-  child1.style.backgroundColor = "black";
-});
+// const child1 = document.getElementById("child1");
+// child1.addEventListener("click", () => {
+//   child1.textContent = "I am clicked";
+// });
+// child1.addEventListener("click", () => {
+//   child1.style.backgroundColor = "black";
+// });
 
-const child2 = document.getElementById("child2");
-child2.addEventListener("dblclick", () => {
-  child2.textContent = "I am clicked";
-});
-child2.addEventListener("dblclick", () => {
-  child2.style.backgroundColor = "black";
-});
-
-const child3 = document.getElementById("child3");
-child3.addEventListener("mouseleave", () => {
-  child3.textContent = "I am clicked";
-});
-child3.addEventListener("mouseleave", () => {
-  child3.style.backgroundColor = "black";
-});
+const parent  = document.getElementById("parent");
+console.log(parent);
