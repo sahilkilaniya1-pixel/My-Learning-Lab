f649680003a51c0c7275846e7ce9b4607143dbb2
Sahil <sahilkilaniya1@gmail.com>
2026-09-12 19:22:59 +0530

    commit

--- Web-Development/Practice-JS/06-Events/index.js
+++ Web-Development/Practice-JS/06-Events/index.js
@@ -4,10 +4,14 @@
 // }
 
 const element = document.getElementById("first");
-element.onclick = function handleClick() {
-  element.textContent = "Strike is coming";
-};
+// element.onclick = function handleClick() {
+//   element.textContent = "Strike is coming";
+// };
 
-element.onclick = function handleClick() {
-  element.textContent = "Iam the Best";
-};
+// element.onclick = function handleClick() {
+//   element.textContent = "Iam the Best";
+// };
+
+element.addEventListener('click', () =>{
+    element.textContent="Strike is Coming"
+})
