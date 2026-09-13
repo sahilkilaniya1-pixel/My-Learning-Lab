c11b0d95b6d5f951d8997a307297475a3372a4c6
Sahil <sahilkilaniya1@gmail.com>
2026-09-13 21:06:05 +0530

    commit

--- Web-Development/Practice-JS/06-Events/index.js
+++ Web-Development/Practice-JS/06-Events/index.js
@@ -18,7 +18,7 @@ element.onclick = function handleClick() {
 
 element.addEventListener("dblclick", () => {
   element.textContent = "Stirke is Coming";
-// });
+});
 // element.addEventListener("dblclick", () => {
 //   element.style.backgroundColor = "SkyBlue";
 // });
