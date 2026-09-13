9a1a340601f778a3187af776f2d133f2243b47f7
Sahil <sahilkilaniya1@gmail.com>
2026-09-13 21:05:19 +0530

    commit

--- Web-Development/Practice-JS/06-Events/index.js
+++ Web-Development/Practice-JS/06-Events/index.js
@@ -9,7 +9,7 @@
 
 const element = document.getElementById("first");
 element.onclick = function handleClick() {
-//   element.textContent = "Strike is coming";
+  element.textContent = "Strike is coming";
 // };
 
 // element.onclick = function handleClick() {
