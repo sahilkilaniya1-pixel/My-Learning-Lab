0226f5105b473275d590223214d2fc06f0a1dd6a
Sahil <sahilkilaniya1@gmail.com>
2026-09-14 00:22:12 +0530

    commit

--- Web-Development/Practice-JS/06-Events/index.js
+++ Web-Development/Practice-JS/06-Events/index.js
@@ -75,5 +75,5 @@ const element = document.getElementById("first");
 // const child = document.getElementById("child");
 // child.addEventListener("click", (e) => {
   // e.stopPropagation();
-  console.log("child is clicked");
+  // console.log("child is clicked");
 });
