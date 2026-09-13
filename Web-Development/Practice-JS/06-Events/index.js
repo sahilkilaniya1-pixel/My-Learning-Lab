039399ba9113ef064919ad649259634f90945453
Sahil <sahilkilaniya1@gmail.com>
2026-09-13 21:03:05 +0530

    commit

--- Web-Development/Practice-JS/06-Events/index.js
+++ Web-Development/Practice-JS/06-Events/index.js
@@ -3,7 +3,7 @@ Events Listener: Listening the events (click)
 Events Action: Strike is coming
 
 function handleClick(){
-//     const element = document.getElementById("first");
+    const element = document.getElementById("first");
 //     element.textContent="Strike is Coming"
 // }
 
