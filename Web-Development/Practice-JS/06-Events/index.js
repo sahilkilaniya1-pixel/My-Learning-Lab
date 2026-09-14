30bfafdddf51f96b9754a3b622bb5de2289bf054
Sahil <sahilkilaniya1@gmail.com>
2026-09-14 21:18:06 +0530

    commit

--- Web-Development/Practice-JS/06-Events/index.js
+++ Web-Development/Practice-JS/06-Events/index.js
@@ -78,8 +78,8 @@ if (child4) {
   });
 }
 
-// if (child5) {
-  child5.addEventListener("mouseleave", (e) => {
+if (child5) {
+  // child5.addEventListener("mouseleave", (e) => {
     e.stopPropagation();
     child5.textContent = "I am clicked";
     child5.style.backgroundColor = "black";
