3559f06d70fb70c08dede1ed0834e648b63a1c68
Sahil <sahilkilaniya1@gmail.com>
2026-09-14 21:15:30 +0530

    commit

--- Web-Development/Practice-JS/06-Events/index.js
+++ Web-Development/Practice-JS/06-Events/index.js
@@ -56,8 +56,8 @@ if (child2) {
     e.stopPropagation();
     child2.textContent = "I am clicked";
     child2.style.backgroundColor = "black";
-    // child2.style.color = "white";
-  });
+    child2.style.color = "white";
+  // });
 }
 
 if (child3) {
