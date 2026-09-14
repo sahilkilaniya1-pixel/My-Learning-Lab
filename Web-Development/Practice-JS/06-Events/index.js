63ef2e8c45c14935ad26a6b73079762df214d135
Sahil <sahilkilaniya1@gmail.com>
2026-09-14 21:10:00 +0530

    commit

--- Web-Development/Practice-JS/06-Events/index.js
+++ Web-Development/Practice-JS/06-Events/index.js
@@ -6,8 +6,8 @@ const element = document.getElementById("first");
 if (element) {
   // Click Event
   element.addEventListener("click", () => {
-    // element.textContent = "Strike is Coming";
-    element.style.backgroundColor = "SkyBlue";
+    element.textContent = "Strike is Coming";
+    // element.style.backgroundColor = "SkyBlue";
     element.style.color = "black";
   });
 
