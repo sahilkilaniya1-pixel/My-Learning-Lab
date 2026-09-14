82b18655b4951c473405681c361f45a7f6542a49
Sahil <sahilkilaniya1@gmail.com>
2026-09-14 21:14:59 +0530

    commit

--- Web-Development/Practice-JS/06-Events/index.js
+++ Web-Development/Practice-JS/06-Events/index.js
@@ -52,8 +52,8 @@ if (child1) {
 }
 
 if (child2) {
-  // child2.addEventListener("dblclick", (e) => {
-    e.stopPropagation();
+  child2.addEventListener("dblclick", (e) => {
+    // e.stopPropagation();
     child2.textContent = "I am clicked";
     child2.style.backgroundColor = "black";
     child2.style.color = "white";
