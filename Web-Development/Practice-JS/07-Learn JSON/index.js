67151f7b166b6f579670b3798598a181d35a1406
Sahil <sahilkilaniya1@gmail.com>
2026-09-15 21:34:39 +0530

    commit

---
+++ Web-Development/Practice-JS/07-Learn JSON/index.js
@@ -0,0 +1,12 @@
+let signup= document.querySelector('.sign')
+let login = document.querySelector('.login')
+let slider = document.querySelector('.slider')
+let formsection = document.querySelector(".form-section")
+slider.addEventListener("click",  () => {
+    slider.classList.add("mouseslider");
+    formsection.classList.add("form-section-move");
+});
+login.addEventListener("clicl", ()=> {
+    slider.classList.remove("moveslider");
+    formSection.classList.remove("form-section-move");
+});
